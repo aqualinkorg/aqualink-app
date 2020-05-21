5a61e46d60e57879f2b29505a4fd2630f5329459
Eric Boucher <eric.p.boucher@gmail.com>
2020-05-20 18:51:45 -0700

    Add reef functions

--- src/reefs/reefs.controller.ts
+++ src/reefs/reefs.controller.ts
@@ -1,30 +1,37 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, UseGuards, AuthGuard } from '@nestjs/common';
 import { ReefDto } from './interfaces/reef.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ReefRepository } from './reefs.repository';
 
 @Controller('reefs')
-export class ReefsController {
-    @Get()
-    getReefs() {
-        return 'we get all reefs';
-    }
+// @UseGuards(AuthGuard('jwt'))
+export class ReefController {
+  constructor(
+      @InjectRepository(ReefRepository) private readonly reefRepository: ReefRepository,
+      ) {}
+      
+      @Post()
+      create(@Body() reefDto: ReefDto) {
+      return this.reefRepository.createReef(reefDto);
+      }
 
-    @Post()
-    create(@Body() reefDto: ReefDto) {
-        return reefDto;
-    }
+  @Get()
+  findAll() {
+    return this.reefRepository.find();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return `we get the reef with the id ${id}`;
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.reefRepository.findOneReef(id);
+  }
 
-    @Put(':id')
-    update(@Param('id') id: string) {
-        return `we update the reef with the id ${id}`;
-    }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() reefDto: ReefDto) {
+    return this.reefRepository.updateReef(id, reefDto);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return `we delete the reef with the id ${id}`;
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.reefRepository.removeReef(id);
+  }
 }
--- src/reefs/reefs.repository.ts
+++ src/reefs/reefs.repository.ts
@@ -7,4 +7,17 @@ export class ReefRepository extends Repository<Reef> {
   createReef = async (reefDto: ReefDto) => {
     return await this.save(reefDto);
   };
+
+  findOneReef = async (id: string) => {
+    return this.findOneOrFail(id);
+  };
+
+  updateReef = async (id: string, reefDto: ReefDto) => {
+    return this.save({ ...reefDto, id: Number(id) });
+  };
+
+  removeReef = async (id: string) => {
+    await this.findOneOrFail(id);
+    return this.delete(id);
+  };
 }
