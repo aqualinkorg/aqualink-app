7c340895a969210e09114c722e412a807b01f44c
Eric Boucher <eric.p.boucher@gmail.com>
2020-05-20 18:30:31 -0700

    Update reef.dto.ts

--- src/reefs/interfaces/reef.dto.ts
+++ src/reefs/interfaces/reef.dto.ts
@@ -1,5 +1,14 @@
 export class ReefDto {
     readonly name: string;
     readonly age: number;
-    readonly breed?: string;
+    readonly  polygon: string;
+    readonly region_id: number;
+    readonly temperature_threshold: number;
+    readonly depth: number;
+    readonly status: string;
+    readonly admin_id: number;
+    readonly video_stream: string;
+    readonly stream_id: number;
+    readonly created_at: Date;
+    readonly updated_at: Date;  
 }
