49d98992aef59e228a41771c6d752844816840ee
Eric Boucher <eric.p.boucher@gmail.com>
2020-05-21 18:09:00 -0700

    Add mock data

--- src/reefs/reefs.controller.ts
+++ src/reefs/reefs.controller.ts
@@ -1,7 +1,11 @@
+import { merge } from 'lodash';
 import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
 import { ReefDto } from './interfaces/reefs.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ReefRepository } from './reefs.repository';
+import reefDetails from '../../mock_response/reefs_id.json'
+import dailyData from '../../mock_response/daily_data.json'
+import surveys from '../../mock_response/survey_data.json'
 
 @Controller('reefs')
 export class ReefsController {
@@ -21,7 +25,27 @@ export class ReefsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.reefRepository.findOneReef(id);
+    return merge(reefDetails, { "id": id });
+    // return this.reefRepository.findOneReef(id);
+  }
+
+  @Get(':id/daily_data')
+  findDailyData(@Param('id') id: string) {
+    return dailyData;
+    // return this.reefRepository.findOneReef(id);
+  }
+
+  @Get(':id/surveys/:poi')
+  findSurveys(@Param('id') id: string, @Param('poi') poi: string) {
+    const filteredSurvey = surveys.map(
+      (survey) => {
+        const images = survey.images.filter(image => image.poi_label_id == parseInt(poi))
+        survey.images = images
+        return survey
+      }
+    )
+    return filteredSurvey;
+    // return this.reefRepository.findOneReef(id);
   }
 
   @Put(':id')
