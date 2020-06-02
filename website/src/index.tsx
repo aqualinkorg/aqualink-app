0201f90961765e6de381783dd4fece71c3b1dcb9
Ben Peters <bencpeters@gmail.com>
2020-06-01 22:39:12 -0700

    Moved Redux store file

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import App from "./layout/App";
-import { store } from "./app/store";
+import { store } from "./store";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
