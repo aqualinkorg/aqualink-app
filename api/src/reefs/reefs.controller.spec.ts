542f8e2ae080c00b72ce8085cbe08d596cc92fcf
Eric Boucher <eric.p.boucher@gmail.com>
2020-06-05 16:53:25 -0700

    Use mock test

--- api/src/reefs/reefs.controller.spec.ts
+++ api/src/reefs/reefs.controller.spec.ts
@@ -1,9 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { ReefsController } from './reefs.controller';
+import { ReefRepository } from './reefs.repository';
 
 describe('Reefs Controller', () => {
   let controller: ReefsController;
 
-  it('Placholder Test', () => {
-    expect(controller)
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      // Mock version of AppService - it will inject the `useValue` as the service in the controller, so you can add
+      // mock implementations there.
+      providers: [{ provide: ReefRepository, useValue: {} }],
+      // You could also provide it the real AppService, but then you'll also have to take care of providing *its*
+      // dependencies too (e.g. with an `imports` block.
+      // providers: [AppService],
+      controllers: [ReefsController],
+    }).compile();
+  
+    controller = module.get<ReefsController>(ReefsController);
+  });
+  
+  it('should be defined', () => {
+     expect(controller).toBeDefined();
   });
 });
