b71c6a126382864d44652367162110a5cba59185
Eric Boucher <eric.p.boucher@gmail.com>
2020-06-05 17:13:57 -0700

    Add circle-ci config (#19)

--- api/src/reefs/reefs.controller.spec.ts
+++ api/src/reefs/reefs.controller.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReefsController } from './reefs.controller';
+import { ReefRepository } from './reefs.repository';
 
 describe('Reefs Controller', () => {
   let controller: ReefsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      // Mock version of ReefRepository - it will inject the `useValue` as the service in the controller, so you can add
+      // mock implementations there.
+      providers: [{ provide: ReefRepository, useValue: {} }],
+      // You could also provide it the real ReefRepository, but then you'll also have to take care of providing *its*
+      // dependencies too (e.g. with an `imports` block.
+      // providers: [ReefRepository],
       controllers: [ReefsController],
     }).compile();
 
