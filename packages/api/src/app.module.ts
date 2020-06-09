21774f771e4e4fe347ac86fc5bf27a76a47914cb
Ben Peters <bencpeters@gmail.com>
2020-06-09 11:00:54 -0700

    Fixed linting errors

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ReefsModule } from './reefs/reefs.module';
 import { configService } from './config/config.service';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
-    ReefsModule
+    ReefsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8080);
+  // eslint-disable-next-line no-console
   console.log(`App listening on port 8080`);
 }
 bootstrap();
