e8d0c6157499b152497d4a0539a4de2c1a4a1fda
Ben Peters <bencpeters@gmail.com>
2020-06-09 12:02:36 -0700

    Updated TypeORM config with a module file, added snake_case naming strategy, added optional DATABASE_URL config option, moved to standard NODE_ENV configuration

---
+++ packages/api/ormconfig.ts
@@ -0,0 +1,54 @@
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { join } from 'path';
+// const { join } = require('path');
+// const { SnakeNamingStrategy } = require('typeorm-naming-strategies')
+
+// dotenv is a dev dependency, so conditionally import it (don't need it in Prod).
+try {
+  // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+  require('dotenv').config();
+} catch {
+  // Pass
+}
+
+const env = process.env.NODE_ENV || 'development';
+
+// If we have a DATABASE_URL, use that
+const connectionInfo = process.env.DATABASE_URL
+  ? { url: process.env.DATABASE_URL }
+  : {
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port:
+        (process.env.POSTGRES_PORT &&
+          parseInt(process.env.POSTGRES_PORT, 10)) ||
+        5432,
+      database: 'aqualink_dev',
+      ...(process.env.POSTGRES_USER && { username: process.env.POSTGRES_USER }),
+      ...(process.env.POSTGRES_PASSWORD && {
+        username: process.env.POSTGRES_PASSWORD,
+      }),
+    };
+
+// Unfortunately, we need to use CommonJS/AMD style exports rather than ES6-style modules for this due to how
+// TypeORM expects the config to be available. Typescript doesn't like this- hence the @ts-ignore.
+// @ts-ignore
+export = {
+  type: 'postgres',
+  ...connectionInfo,
+  // We don't want to auto-synchronize production data - we should deliberately run migrations.
+  synchronize: env === 'development',
+  logging: false,
+  namingStrategy: new SnakeNamingStrategy(),
+  entities: [
+    // Needed to get a TS context on entity imports.
+    // See
+    // https://stackoverflow.com/questions/59435293/typeorm-entity-in-nestjs-cannot-use-import-statement-outside-a-module
+    join(__dirname, 'src/**', '*.entity.ts'),
+  ],
+  migrations: [join(__dirname, 'migration/**', '*.ts')],
+  subscribers: [join(__dirname, 'subscriber/**', '*.ts')],
+  cli: {
+    migrationsDir: 'migration',
+    subscribersDir: 'subscriber',
+  },
+};
