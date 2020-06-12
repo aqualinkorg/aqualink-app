d12a743fe51e9a4c5c0efd154714f13f1d20c223
Thanasis Leraj <tleraj@programize.com>
2020-06-12 13:32:05 +0300

    Remove unused type definitions

--- packages/website/src/store/configure.ts
+++ packages/website/src/store/configure.ts
@@ -1,4 +1,4 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
 import reducer from "./reducer";
 
@@ -7,13 +7,6 @@ export const store = configureStore({ reducer });
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
-
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
 
 export type CreateAsyncThunkTypes = {
   dispatch: AppDispatch;
