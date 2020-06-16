8d5d1d40a9bf160ad689c9ee6eb1cf89ff198baa
Thanasis Leraj <tleraj@programize.com>
2020-06-16 12:15:37 +0300

    Disable map zoom out of initial bounds

--- packages/website/src/routes/ReefRoutes/Reef/Map/index.tsx
+++ packages/website/src/routes/ReefRoutes/Reef/Map/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Map, TileLayer, Polygon } from "react-leaflet";
 import { withStyles, WithStyles, createStyles } from "@material-ui/core";
 
@@ -6,16 +6,22 @@ import { Reef } from "../../../../store/Reefs/types";
 import { mapBounds } from "../../../../helpers/mapBounds";
 
 const ReefMap = ({ polygon, classes }: ReefMapProps) => {
+  const mapRef = useRef<Map>(null);
+
+  useEffect(() => {
+    const { current } = mapRef;
+    if (current && current.leafletElement) {
+      const map = current.leafletElement;
+      map.fitBounds(mapBounds(polygon.coordinates), { padding: [150, 150] });
+      const zoom = map.getZoom();
+      if (zoom < Infinity) {
+        map.setMinZoom(zoom);
+      }
+    }
+  }, [mapRef, polygon]);
+
   return (
-    <Map
-      dragging={false}
-      className={classes.map}
-      bounds={mapBounds(polygon.coordinates)}
-      boundsOptions={{ padding: [150, 150] }}
-      zoomControl={false}
-      doubleClickZoom={false}
-      scrollWheelZoom={false}
-    >
+    <Map ref={mapRef} dragging={false} className={classes.map}>
       <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" />
       <Polygon positions={polygon.coordinates} />
     </Map>
