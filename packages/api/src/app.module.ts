3bda76d4cb96b23b8c4533ab2722380fcabba055
Eric Boucher <eric.p.boucher@gmail.com>
2020-06-23 23:16:21 -0700

    Re-add ReefsModule

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { configService } from './config/config.service';
+import { ReefsModule } from './reefs/reefs.module';
 
 @Module({
   imports: [
+    ReefsModule,
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
   ],
   controllers: [AppController],
