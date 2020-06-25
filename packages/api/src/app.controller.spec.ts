9f482eac9d0fe9a51bc96e2bb892ba3e3595a720
Eric Boucher <eric.p.boucher@gmail.com>
2020-06-25 01:19:37 -0700

    Revert "Update app.controller.spec.ts"
    This reverts commit c9cd0bacd0f6bfd292aba123cf146d956ef034f5.

--- packages/api/src/app.controller.spec.ts
+++ packages/api/src/app.controller.spec.ts
@@ -13,8 +13,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Welcome to Aqualink!"', () => {
-      expect(appController.getHello()).toBe('Welcome to Aqualink!');
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
     });
   });
 });
