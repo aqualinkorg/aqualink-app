e2c1572db556f060cc5260102cbb602928a0ee5a
Ben Peters <bencpeters@gmail.com>
2020-07-01 10:08:54 -0700

    removed unused AppController

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
 import { configService } from './config/config.service';
 import { ReefsModule } from './reefs/reefs.module';
 import { ReefApplicationsModule } from './reef-applications/reef-applications.module';
@@ -13,6 +12,5 @@ import { RegionsModule } from './regions/regions.module';
     RegionsModule,
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
   ],
-  controllers: [AppController],
 })
 export class AppModule {}
