305c2ea5659f4fdbef9bd7058a28ec5f61936de0
Ben Peters <bencpeters@gmail.com>
2020-07-01 10:13:08 -0700

    Removed non application-related paths from API

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
-import { ReefsModule } from './reefs/reefs.module';
 import { ReefApplicationsModule } from './reef-applications/reef-applications.module';
-import { RegionsModule } from './regions/regions.module';
 
 @Module({
   imports: [
-    ReefsModule,
     ReefApplicationsModule,
-    RegionsModule,
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
   ],
 })
