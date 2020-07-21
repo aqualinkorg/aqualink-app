4c52a237ca63954aba84a4621267d789b5fcaa35
Eric Boucher <eric.p.boucher@gmail.com>
2020-07-20 22:23:51 -0700

    Add no duplicate constraint

--- packages/api/migration/1594865084047-makeDailyDataNullable.ts
+++ packages/api/migration/1594865084047-makeDailyDataNullable.ts
@@ -19,9 +19,13 @@ export class makeDailyDataNullable1594865084047 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "daily_data" ALTER COLUMN "max_wind_speed" DROP NOT NULL`);
         await queryRunner.query(`ALTER TABLE "daily_data" ALTER COLUMN "avg_wind_speed" DROP NOT NULL`);
         await queryRunner.query(`ALTER TABLE "daily_data" ALTER COLUMN "wind_direction" DROP NOT NULL`);
+        await queryRunner.query(`CREATE EXTENSION btree_gist`);
+        await queryRunner.query(`ALTER TABLE "daily_data" ADD CONSTRAINT no_duplicated_date EXCLUDE USING GIST (reef_id WITH =, DATE(date) WITH =)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "daily_data" DROP CONSTRAINT no_duplicate_date`);
+        await queryRunner.query(`DROP EXTENSION btree_gist`);
         await queryRunner.query(`ALTER TABLE "daily_data" ALTER COLUMN "wind_direction" SET NOT NULL`);
         await queryRunner.query(`ALTER TABLE "daily_data" ALTER COLUMN "avg_wind_speed" SET NOT NULL`);
         await queryRunner.query(`ALTER TABLE "daily_data" ALTER COLUMN "max_wind_speed" SET NOT NULL`);
