9ffeb0b99ec7891789b2c40404c1affae387ba51
Thanasis Leraj <tleraj@programize.com>
2020-07-28 15:40:10 +0300

    Edit reef info back button

--- packages/website/src/routes/ReefRoutes/Reef/ReefInfo/index.tsx
+++ packages/website/src/routes/ReefRoutes/Reef/ReefInfo/index.tsx
@@ -8,7 +8,7 @@ import {
   WithStyles,
   createStyles,
 } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import ArrowBack from "@material-ui/icons/ArrowBack";
 
 const ReefNavBar = ({
@@ -16,64 +16,74 @@ const ReefNavBar = ({
   lastSurvey,
   managerName,
   classes,
-}: ReefNavBarProps) => (
-  <Grid
-    className={classes.root}
-    item
-    container
-    justify="space-around"
-    alignItems="center"
-    xs={12}
-  >
-    <Grid item xs={5}>
-      <Grid alignItems="center" direction="row" container spacing={1}>
-        <Grid item>
-          <Link style={{ color: "inherit" }} to="/reefs">
-            <IconButton edge="start" color="primary" aria-label="menu">
+}: ReefNavBarProps) => {
+  const history = useHistory();
+
+  return (
+    <Grid
+      className={classes.root}
+      item
+      container
+      justify="space-around"
+      alignItems="center"
+      xs={12}
+    >
+      <Grid item xs={5}>
+        <Grid alignItems="center" direction="row" container spacing={1}>
+          <Grid item>
+            <IconButton
+              edge="start"
+              onClick={history.goBack}
+              color="primary"
+              aria-label="menu"
+            >
               <ArrowBack />
             </IconButton>
-          </Link>
+          </Grid>
+          {reefName && lastSurvey && (
+            <Grid item xs={7} direction="column" container>
+              <Grid item>
+                <Typography variant="h4">{reefName}</Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1">05/12/20 8:16AM PST</Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1">{`Last surveyed: ${lastSurvey}`}</Typography>
+              </Grid>
+            </Grid>
+          )}
         </Grid>
-        {reefName && lastSurvey && (
-          <Grid item xs={7} direction="column" container>
-            <Grid item>
-              <Typography variant="h4">{reefName}</Typography>
+      </Grid>
+      <Grid item xs={5}>
+        {managerName && (
+          <Grid container justify="flex-end" alignItems="center">
+            <Grid className={classes.managerInfo} item>
+              <Grid container direction="column" alignItems="flex-end">
+                <Typography variant="subtitle1">
+                  {`Managed by: ${managerName}`}
+                </Typography>
+                <Link
+                  style={{ color: "inherit", textDecoration: "none" }}
+                  to="/"
+                >
+                  <Typography color="primary" variant="subtitle2">
+                    Contact Manager
+                  </Typography>
+                </Link>
+              </Grid>
             </Grid>
             <Grid item>
-              <Typography variant="subtitle1">05/12/20 8:16AM PST</Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle1">{`Last surveyed: ${lastSurvey}`}</Typography>
+              <Grid container>
+                <Avatar />
+              </Grid>
             </Grid>
           </Grid>
         )}
       </Grid>
     </Grid>
-    <Grid item xs={5}>
-      {managerName && (
-        <Grid container justify="flex-end" alignItems="center">
-          <Grid className={classes.managerInfo} item>
-            <Grid container direction="column" alignItems="flex-end">
-              <Typography variant="subtitle1">
-                {`Managed by: ${managerName}`}
-              </Typography>
-              <Link style={{ color: "inherit", textDecoration: "none" }} to="/">
-                <Typography color="primary" variant="subtitle2">
-                  Contact Manager
-                </Typography>
-              </Link>
-            </Grid>
-          </Grid>
-          <Grid item>
-            <Grid container>
-              <Avatar />
-            </Grid>
-          </Grid>
-        </Grid>
-      )}
-    </Grid>
-  </Grid>
-);
+  );
+};
 
 const styles = () =>
   createStyles({
