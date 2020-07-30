45c151350b439adc738a5c2bc8dad410b25e9ab8
Vicky Kontoura <vickykon@programize.com>
2020-07-30 15:00:23 +0300

    Fix module dependencies

--- packages/api/src/regions/regions.module.ts
+++ packages/api/src/regions/regions.module.ts
@@ -4,12 +4,11 @@ import { RegionsController } from './regions.controller';
 import { RegionsService } from './regions.service';
 import { Region } from './regions.entity';
 import { EntityExists } from '../validations/entity-exists.constraint';
-import { UsersService } from '../users/users.service';
-import { User } from '../users/users.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Region, User])],
+  imports: [AuthModule, TypeOrmModule.forFeature([Region])],
   controllers: [RegionsController],
-  providers: [RegionsService, UsersService, EntityExists],
+  providers: [RegionsService, EntityExists],
 })
 export class RegionsModule {}
