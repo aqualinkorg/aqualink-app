adf079558d0cf3dcd43da61a5449f5b607b5a136
Eric Boucher <eric.p.boucher@gmail.com>
2020-08-06 10:31:59 -0700

    Specify conversion mechanism

--- packages/api/migration/1594903107174-UpdateUserLocationType.ts
+++ packages/api/migration/1594903107174-UpdateUserLocationType.ts
@@ -4,7 +4,7 @@ export class UpdateUserLocationType1594903107174 implements MigrationInterface {
     name = 'UpdateUserLocationType1594903107174'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "location" TYPE geometry(Point,4326)`);
+        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "location" TYPE geometry(Point,4326) USING location::geometry(Point,4326)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
