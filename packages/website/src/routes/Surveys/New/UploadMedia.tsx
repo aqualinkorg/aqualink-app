b65179f532084ba4d226deec25736619ca41e6ac
Thanasis Leraj <tleraj@programize.com>
2020-08-13 16:28:43 +0300

    Create card for each uploaded image

--- packages/website/src/routes/Surveys/New/UploadMedia.tsx
+++ packages/website/src/routes/Surveys/New/UploadMedia.tsx
@@ -6,19 +6,49 @@ import {
   IconButton,
   Grid,
   Typography,
+  Paper,
+  CardMedia,
+  Theme,
 } from "@material-ui/core";
 import { ArrowBack, CloudUploadOutlined } from "@material-ui/icons";
 import Dropzone from "react-dropzone";
 
 const UploadMedia = ({ reefName, changeTab, classes }: UploadMediaProps) => {
   const [files, setFiles] = useState<File[]>([]);
+  const [previews, setPreviews] = useState<string[]>([]);
 
   const handleFileDrop = useCallback(
     (acceptedFiles: File[]) => {
       setFiles([...files, ...acceptedFiles]);
+      setPreviews([
+        ...previews,
+        ...acceptedFiles.map((file) => URL.createObjectURL(file)),
+      ]);
     },
-    [files]
+    [files, previews]
   );
+
+  const fileCards = previews.map((preview) => {
+    return (
+      <Grid style={{ marginTop: "2rem" }} key={preview} container item xs={12}>
+        {/* <img key={preview} src={preview} alt="uploaded-media" /> */}
+        <Paper elevation={0} className={classes.mediaCardWrapper}>
+          <Grid
+            style={{ height: "100%" }}
+            container
+            alignItems="center"
+            justify="space-between"
+            item
+            xs={12}
+          >
+            <Grid style={{ height: "100%" }} item xs={3}>
+              <CardMedia className={classes.cardImage} image={preview} />
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+    );
+  });
 
   return (
     <Grid className={classes.root} container justify="center" item xs={12}>
@@ -40,7 +70,10 @@ const UploadMedia = ({ reefName, changeTab, classes }: UploadMediaProps) => {
         </Grid>
       </Grid>
       <Grid container justify="center" item xs={4}>
-        <Dropzone accept="image/png" onDrop={handleFileDrop}>
+        <Dropzone
+          accept="image/png, image/jpeg, image/gif"
+          onDrop={handleFileDrop}
+        >
           {({ getRootProps, getInputProps }) => (
             <Grid
               container
@@ -65,11 +98,14 @@ const UploadMedia = ({ reefName, changeTab, classes }: UploadMediaProps) => {
           )}
         </Dropzone>
       </Grid>
+      <Grid style={{ marginBottom: "2rem" }} container item xs={9}>
+        {fileCards}
+      </Grid>
     </Grid>
   );
 };
 
-const styles = () =>
+const styles = (theme: Theme) =>
   createStyles({
     root: {
       marginTop: "2rem",
@@ -82,6 +118,23 @@ const styles = () =>
       backgroundColor: "#fafafa",
       height: "8rem",
       width: "100%",
+    },
+    mediaCardWrapper: {
+      width: "100%",
+      backgroundColor: theme.palette.primary.light,
+      border: 1,
+      borderStyle: "solid",
+      borderColor: "#dddddd",
+      borderRadius: 2,
+      height: "18rem",
+    },
+    cardImage: {
+      height: "100%",
+      width: "100%",
+      borderRadius: "2px 0 0 2px",
+      display: "flex",
+      alignItems: "flex-end",
+      justifyContent: "flex-end",
     },
   });
 
