f1fb1872aca381dc6503ab904edc3acd26f552a4
Thanasis Leraj <tleraj@programize.com>
2020-08-13 18:42:13 +0300

    Add card editing/removal capability

---
+++ packages/website/src/constants/uploadDropdowns.ts
@@ -0,0 +1,60 @@
+const surveyPointOptions = [
+  {
+    key: "left",
+    value: "Left reef mass",
+  },
+  {
+    key: "right",
+    value: "Right reef mass",
+  },
+  {
+    key: "center",
+    value: "Center area",
+  },
+  {
+    key: "A",
+    value: "Point of interest A",
+  },
+  {
+    key: "B",
+    value: "Point of interest B",
+  },
+  {
+    key: "C",
+    value: "Point of interest C",
+  },
+  {
+    key: "D",
+    value: "Point of interest D",
+  },
+];
+
+const observationOptions = [
+  {
+    key: "healthy",
+    value: "Appears healthy",
+  },
+  {
+    key: "possible-disease",
+    value: "Showing possible signs of disease",
+  },
+  {
+    key: "evident-disease",
+    value: "Signs of disease are evident",
+  },
+  {
+    key: "mortality",
+    value: "Signs of mortality are evident",
+  },
+  {
+    key: "environmental",
+    value: "Signs of environmental disturbance are evident (e.g.,storm damage)",
+  },
+  {
+    key: "anthropogenic",
+    value:
+      "Signs of anthropogenic disturbance are evident (e.g.,fishing gear, trampling)",
+  },
+];
+
+export { surveyPointOptions, observationOptions };
