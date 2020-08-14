373c9957218a3d6a5aa90aeba6f6bead3dd54f79
Eric Boucher <eric.p.boucher@gmail.com>
2020-08-14 10:35:19 -0700

    Picky dailyData updates

--- packages/api/src/workers/dailyData.ts
+++ packages/api/src/workers/dailyData.ts
@@ -1,5 +1,5 @@
 /** Worker to process daily data for all reefs. */
-import { sum } from 'lodash';
+import { pickBy, sum } from 'lodash';
 import { Connection } from 'typeorm';
 import { Point } from 'geojson';
 import { Reef } from '../reefs/reefs.entity';
@@ -180,12 +180,13 @@ export async function getReefsDailyData(connection: Connection, date: Date) {
     } catch (err) {
       // Update instead of insert
       if (err.constraint === 'no_duplicated_date') {
+        const filteredReef = pickBy(reef);
         dailyDataRepository.update(
           {
             reef,
             date: entity.date,
           },
-          entity,
+          filteredReef,
         );
         // eslint-disable-next-line no-continue
         continue;
