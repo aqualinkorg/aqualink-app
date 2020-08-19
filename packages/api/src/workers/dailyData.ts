81cae769347a2ebb9e5bd0d544b9d28c9c8cfcb4
Eric Boucher <eric.p.boucher@gmail.com>
2020-08-19 14:46:29 -0700

    Use omitBy Nil instead

--- packages/api/src/workers/dailyData.ts
+++ packages/api/src/workers/dailyData.ts
@@ -1,5 +1,5 @@
 /** Worker to process daily data for all reefs. */
-import { pickBy, sum } from 'lodash';
+import { isNil, omitBy, sum } from 'lodash';
 import { Connection } from 'typeorm';
 import { Point } from 'geojson';
 import { Reef } from '../reefs/reefs.entity';
@@ -180,7 +180,7 @@ export async function getReefsDailyData(connection: Connection, date: Date) {
     } catch (err) {
       // Update instead of insert
       if (err.constraint === 'no_duplicated_date') {
-        const filteredData = pickBy(entity);
+        const filteredData = omitBy(entity, isNil);
 
         dailyDataRepository.update(
           {
