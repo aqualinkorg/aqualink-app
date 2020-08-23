8834845860ec7c556f21ce27e4ed2664c7c7c403
Eric Boucher <eric.p.boucher@gmail.com>
2020-08-23 15:00:48 -0700

    Fix tests

--- packages/website/src/routes/Homepage/ReefTable/SelectedReefCard/index.test.tsx
+++ packages/website/src/routes/Homepage/ReefTable/SelectedReefCard/index.test.tsx
@@ -15,6 +15,54 @@ jest.mock("react-chartjs-2", () => ({
 }));
 
 test("renders as expected", () => {
+  process.env.REACT_APP_FEATURED_REEF_ID = "2";
+  const reef: Reef = {
+    id: 2,
+    name: "",
+    polygon: {
+      coordinates: [0, 0],
+      type: "Point",
+    },
+    maxMonthlyMean: 0,
+    depth: 0,
+    status: 0,
+    videoStream: null,
+    region: "",
+    admin: null,
+    stream: null,
+    dailyData: [
+      {
+        id: 171,
+        date: "2020-07-01T16:40:19.842Z",
+        minBottomTemperature: 37,
+        maxBottomTemperature: 39,
+        avgBottomTemperature: 38,
+        degreeHeatingDays: 34,
+        surfaceTemperature: 29,
+        satelliteTemperature: 23,
+        minWaveHeight: 2,
+        maxWaveHeight: 4,
+        avgWaveHeight: 3,
+        waveDirection: 205,
+        wavePeriod: 28,
+        minWindSpeed: 3,
+        maxWindSpeed: 5,
+        avgWindSpeed: 4,
+        windDirection: 229,
+      },
+    ],
+  };
+
+  const { container } = render(
+    <Router>
+      <SelectedReefCard reef={reef} />
+    </Router>
+  );
+  expect(container).toMatchSnapshot();
+});
+
+test("renders loading as expected", () => {
+  process.env.REACT_APP_FEATURED_REEF_ID = "4";
   const reef: Reef = {
     id: 2,
     name: "",
