4fad4a0806e271bf32bb01b8d06603b8b5bb0179
Eric Boucher <eric.p.boucher@gmail.com>
2020-08-31 23:10:37 +0200

    Update tests

--- packages/api/src/utils/sofar.test.ts
+++ packages/api/src/utils/sofar.test.ts
@@ -31,14 +31,5 @@ test('It processes Sofar Spotter API for daily data.', async () => {
     new Date('2020-07-07'),
   );
 
-  expect(values).toEqual([
-    { timestamp: '2020-07-06T12:00:00.000Z', value: 29.3450012207031 },
-    { timestamp: '2020-07-06T15:00:00.000Z', value: 29.4020004272461 },
-    { timestamp: '2020-07-06T18:00:00.000Z', value: 29.3900012969971 },
-    { timestamp: '2020-07-06T21:00:00.000Z', value: 29.3250007629395 },
-    { timestamp: '2020-07-07T00:00:00.000Z', value: 29.4540004730225 },
-    { timestamp: '2020-07-07T03:00:00.000Z', value: 29.4799995422363 },
-    { timestamp: '2020-07-07T06:00:00.000Z', value: 29.4029998779297 },
-    { timestamp: '2020-07-07T09:00:00.000Z', value: 29.423999786377 },
-  ]);
+  expect(values).toEqual({ surfaceTemperature: 20, bottomTemperature: [0] });
 });
