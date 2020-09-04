1def45dec7baae0c2bf0856ed3e4d94cc5f72453
Eric Boucher <eric.p.boucher@gmail.com>
2020-09-04 12:25:50 +0200

    Fix tests

--- packages/website/src/routes/ReefRoutes/Reef/ReefInfo/index.test.tsx
+++ packages/website/src/routes/ReefRoutes/Reef/ReefInfo/index.test.tsx
@@ -6,7 +6,12 @@ import ReefNavBar from ".";
 test("renders as expected", () => {
   const { container } = render(
     <Router>
-      <ReefNavBar reefName="" managerName="" lastSurvey="May 10, 2020" />
+      <ReefNavBar
+        reefName=""
+        managerName=""
+        lastSurvey="May 10, 2020"
+        lastDailyDataDate="May 10, 2020"
+      />
     </Router>
   );
   expect(container).toMatchSnapshot();
