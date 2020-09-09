0453adab07025389e64f854d6b44ab01d29f6a60
Ben Peters <bencpeters@gmail.com>
2020-09-08 18:31:58 -0700

    Allow SVG imports as components

--- packages/website/src/custom.d.ts
+++ packages/website/src/custom.d.ts
@@ -1,10 +1,3 @@
-// Allow svg imports in .tsx
-// https://stackoverflow.com/questions/44717164/unable-to-import-svg-files-in-typescript
-declare module "*.svg" {
-  const content: string;
-  export default content;
-}
-
 declare module "*.png";
 declare module "*.jpg";
 
