6ea9d3bc2448b1c5e08c1926a9914e6f4b1b88e4
Thanasis Leraj <tleraj@programize.com>
2020-09-09 17:50:25 +0300

    Add loading progress bar

--- packages/website/src/routes/ReefRoutes/Reef/Surveys/DeleteButton.tsx
+++ packages/website/src/routes/ReefRoutes/Reef/Surveys/DeleteButton.tsx
@@ -12,6 +12,7 @@ import {
   IconButton,
   Button,
   Collapse,
+  LinearProgress,
 } from "@material-ui/core";
 import Alert from "@material-ui/lab/Alert";
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
@@ -33,6 +34,7 @@ const DeleteButton = ({
   const [open, setOpen] = useState<boolean>(false);
   const [alertOpen, setAlertOpen] = useState<boolean>(false);
   const [alertText, setAlertText] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -46,6 +48,7 @@ const DeleteButton = ({
 
   const onSurveyDelete = () => {
     if (surveyId && user && user.token) {
+      setLoading(true);
       surveyServices
         .deleteSurvey(surveyId, user.token)
         .then(() => {
@@ -54,7 +57,8 @@ const DeleteButton = ({
         .catch((error) => {
           setAlertOpen(true);
           setAlertText(error.message);
-        });
+        })
+        .finally(() => setLoading(false));
     }
   };
 
@@ -73,6 +77,7 @@ const DeleteButton = ({
           </Typography>
         </DialogTitle>
         <DialogContent className={classes.dialogContent}>
+          {loading && <LinearProgress />}
           <Collapse className={classes.alert} in={alertOpen}>
             <Alert
               severity="error"
