61241921b5b97e18d9316fb2e58f1a1d97685d30
Thanasis Leraj <tleraj@programize.com>
2020-09-10 12:18:58 +0300

    Remove inline styles from signIn/register dialogs

---
+++ packages/website/src/routes/Homepage/styles.ts
@@ -0,0 +1,34 @@
+import theme from "../../layout/App/theme";
+
+const styles = {
+  closeButton: {
+    color: theme.palette.primary.light,
+  },
+  dialogHeader: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  dialogHeaderSecondPart: {
+    color: "#8AC6DE",
+  },
+  dialogContentTitle: {
+    marginTop: "0.5rem",
+  },
+  form: {
+    width: "100%",
+  },
+  textFieldWrapper: {
+    margin: "1rem 0 1rem 0",
+  },
+  textField: {
+    color: "black",
+  },
+  formText: {
+    color: theme.palette.grey[500],
+    fontWeight: 400,
+  },
+  button: {
+    marginBottom: "0.5rem",
+  },
+};
+
+export default styles;
