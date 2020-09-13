ef46d4bfc09f7d4e20d8a6790f4403da501b6e15
Eric Boucher <eric.p.boucher@gmail.com>
2020-09-13 17:01:36 +0200

    Disable videos (#190)

--- packages/website/src/routes/Surveys/View/SurveyDetails.tsx
+++ packages/website/src/routes/Surveys/View/SurveyDetails.tsx
@@ -7,10 +7,7 @@ import {
   Grid,
   Typography,
 } from "@material-ui/core";
-import {
-  getNumberOfImages,
-  getNumberOfVideos,
-} from "../../../helpers/surveyMedia";
+import { getNumberOfImages } from "../../../helpers/surveyMedia";
 
 import type { Reef } from "../../../store/Reefs/types";
 import type { SurveyState } from "../../../store/Survey/types";
@@ -63,22 +60,6 @@ const SurveyDetails = ({ reef, survey, classes }: SurveyDetailsProps) => {
                 className={classes.inlineText}
               >
                 IMAGES
-              </Typography>
-            </Grid>
-            <Grid item xs={12} md={3}>
-              <Typography
-                color="primary"
-                variant="h4"
-                className={classes.inlineText}
-              >
-                {survey.surveyPoints && getNumberOfVideos(survey.surveyPoints)}
-              </Typography>
-              <Typography
-                color="initial"
-                variant="h6"
-                className={classes.inlineText}
-              >
-                VIDEOS
               </Typography>
             </Grid>
           </Grid>
