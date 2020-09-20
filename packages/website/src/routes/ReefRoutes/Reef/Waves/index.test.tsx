c81c3026c6b89d094f9b0de25fc1a4f058f9666e
Eric Boucher <eric.p.boucher@gmail.com>
2020-09-20 21:15:34 +0200

    Update tests

--- packages/website/src/routes/ReefRoutes/Reef/Waves/index.test.tsx
+++ packages/website/src/routes/ReefRoutes/Reef/Waves/index.test.tsx
@@ -4,27 +4,42 @@ import { render } from "@testing-library/react";
 import Waves from ".";
 
 test("renders as expected", () => {
-  const dailyData = [
-    {
-      id: 1,
-      date: "2020-07-01T14:25:18.008Z",
-      minBottomTemperature: 25,
-      maxBottomTemperature: 27,
-      avgBottomTemperature: 26,
-      degreeHeatingDays: 29,
-      surfaceTemperature: 36,
-      satelliteTemperature: 20,
-      minWaveHeight: 1,
-      maxWaveHeight: 3,
-      avgWaveHeight: 2,
-      waveDirection: 136,
-      wavePeriod: 15,
-      minWindSpeed: 3,
-      maxWindSpeed: 5,
-      avgWindSpeed: 4,
-      windDirection: 96,
+  const liveData = {
+    reef: { id: 1 },
+    date: "2020-07-01T14:25:18.008Z",
+    bottomTemperature: {
+      value: 25,
+      timestamp: "2020-07-01T14:25:18.008Z",
     },
-  ];
-  const { container } = render(<Waves dailyData={dailyData} />);
+    satelliteTemperature: {
+      value: 26,
+      timestamp: "2020-07-01T14:25:18.008Z",
+    },
+    degreeHeatingDays: {
+      value: 29,
+      timestamp: "2020-07-01T14:25:18.008Z",
+    },
+    waveHeight: {
+      value: 3,
+      timestamp: "2020-07-01T14:25:18.008Z",
+    },
+    waveDirection: {
+      value: 136,
+      timestamp: "2020-07-01T14:25:18.008Z",
+    },
+    wavePeriod: {
+      value: 15,
+      timestamp: "2020-07-01T14:25:18.008Z",
+    },
+    windSpeed: {
+      value: 5,
+      timestamp: "2020-07-01T14:25:18.008Z",
+    },
+    windDirection: {
+      value: 96,
+      timestamp: "2020-07-01T14:25:18.008Z",
+    },
+  };
+  const { container } = render(<Waves liveData={liveData} />);
   expect(container).toMatchSnapshot();
 });
