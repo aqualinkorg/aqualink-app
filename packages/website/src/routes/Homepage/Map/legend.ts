0b308df7cc216c8ca25879d71b91672436e31f81
Thanasis Leraj <tleraj@programize.com>
2020-09-21 12:07:22 +0300

    Add fahrenheit degree legend

--- packages/website/src/routes/Homepage/Map/legend.ts
+++ packages/website/src/routes/Homepage/Map/legend.ts
@@ -1,11 +1,15 @@
 import L from "leaflet";
-import legendImage from "../../../assets/legend.png";
+import celsiusLegend from "../../../assets/celsiusLegend.png";
+import fahrenheitLegend from "../../../assets/fahrenheitLegend.png";
 
 const legend = new L.Control({ position: "bottomleft" });
 
 legend.onAdd = () => {
-  const div = L.DomUtil.create("div", "info legend");
-  div.innerHTML = `<img src="${legendImage}" alt="surface-temperature-legend" />`;
+  const div = L.DomUtil.create("div", "info-legend");
+  div.innerHTML = `
+    <img src="${celsiusLegend}" alt="surface-temperature-legend" />
+    <img src="${fahrenheitLegend}" alt="surface-temperature-legend" />
+  `;
 
   return div;
 };
