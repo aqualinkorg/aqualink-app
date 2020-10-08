7317430c6ea6d0e9bcf348f7d38e3dd03375a9a1
Antonios Valmas <avalmas@programize.com>
2020-10-08 13:33:52 +0300

    Refactor google cloud utils

--- packages/api/src/google-cloud/google-cloud.service.ts
+++ packages/api/src/google-cloud/google-cloud.service.ts
@@ -7,11 +7,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Storage } from '@google-cloud/storage';
 import { Repository } from 'typeorm';
 import { SurveyMedia } from '../surveys/survey-media.entity';
+import { getFileFromURL } from '../utils/google-cloud.utils';
 
 @Injectable()
 export class GoogleCloudService {
   private logger = new Logger('GCS');
   private storage: Storage;
+
+  private readonly GCS_BUCKET = process.env.GCS_BUCKET;
+  private readonly STORAGE_FOLDER = process.env.STORAGE_FOLDER;
+  private readonly GCS_KEYFILE = process.env.GCS_KEYFILE;
+  private readonly GCS_PROJECT = process.env.GCS_PROJECT;
 
   constructor(
     @InjectRepository(SurveyMedia)
@@ -19,14 +25,14 @@ export class GoogleCloudService {
   ) {
     this.storage = new Storage({
       autoRetry: true,
-      keyFilename: process.env.GCS_KEYFILE,
-      projectId: process.env.GCS_PROJECT,
+      keyFilename: this.GCS_KEYFILE,
+      projectId: this.GCS_PROJECT,
       maxRetries: 3,
     });
   }
 
   public async findDanglingFiles(): Promise<string[]> {
-    if (!process.env.GCS_BUCKET || !process.env.STORAGE_FOLDER) {
+    if (!this.GCS_BUCKET || !this.STORAGE_FOLDER) {
       this.logger.error(
         'GCS_BUCKET or STORAGE_FOLDER variable has not been initialized',
       );
@@ -38,14 +44,12 @@ export class GoogleCloudService {
     });
 
     const mediaSet = new Set(
-      surveyMedia.map(
-        (media) => media.url.split(`${process.env.GCS_BUCKET}/`)[1],
-      ),
+      surveyMedia.map((media) => getFileFromURL(media.url)),
     );
 
     const fileResponse = await this.storage
-      .bucket(process.env.GCS_BUCKET)
-      .getFiles({ directory: process.env.STORAGE_FOLDER });
+      .bucket(this.GCS_BUCKET)
+      .getFiles({ directory: this.STORAGE_FOLDER });
 
     return fileResponse[0]
       .filter((file) => !mediaSet.has(file.name))
@@ -65,13 +69,13 @@ export class GoogleCloudService {
   }
 
   public async deleteFile(file: string) {
-    if (!process.env.GCS_BUCKET) {
+    if (!this.GCS_BUCKET) {
       this.logger.error('GCS_BUCKET variable has not been initialized');
       throw new InternalServerErrorException();
     }
 
     try {
-      await this.storage.bucket(process.env.GCS_BUCKET).file(file).delete();
+      await this.storage.bucket(this.GCS_BUCKET).file(file).delete();
     } catch (error) {
       this.logger.error(`Failed to delete the file ${file}: `, error);
       throw new InternalServerErrorException();
---
+++ packages/api/src/utils/google-cloud.utils.ts
@@ -0,0 +1,10 @@
+import { InternalServerErrorException } from '@nestjs/common';
+
+const { GCS_BUCKET } = process.env;
+
+export const getFileFromURL = (url: string) => {
+  if (!GCS_BUCKET) {
+    throw new InternalServerErrorException('GCS_BUCKET variable is not set');
+  }
+  return url.split(`${GCS_BUCKET}/`)[1];
+};
