a493d719832f9222a6ae4f26652b750d088418d8
Eric Boucher <eric.p.boucher@gmail.com>
2020-10-13 01:46:55 +0200

    mock date picker

--- packages/website/src/routes/Apply/Form/index.test.tsx
+++ packages/website/src/routes/Apply/Form/index.test.tsx
@@ -3,6 +3,10 @@ import { render } from "@testing-library/react";
 
 import Form from ".";
 
+jest.mock("@material-ui/pickers", () => ({
+  MuiPickersUtilsProvider: () => "Mock-MuiPickersUtilsProvider",
+}));
+
 test("renders as expected", () => {
   const { container } = render(
     <Form reefName="Mock Reef" agreed handleFormSubmit={jest.fn()} />
