7620dfe03a702619057617298696a96148918664
avalmas-programize <69854742+avalmas-programize@users.noreply.github.com>
2020-10-19 13:08:24 +0300

    Add spotter exclusion dates (#306)

---
+++ packages/api/src/reefs/exclusion-dates.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+@Entity()
+@Unique('no_duplicates_start_date', ['spotterId', 'startDate'])
+@Unique('no_duplicate_end_date', ['spotterId', 'endDate'])
+export class ExclusionDates {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  spotterId: string;
+
+  // The start of the exclusive range. Inclusive
+  @Column()
+  startDate: Date;
+
+  // The end of the exclusive range. Inclusive
+  @Column()
+  endDate: Date;
+}
