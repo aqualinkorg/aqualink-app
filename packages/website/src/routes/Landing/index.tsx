7d2421c4ffa749c7de001b177d37cd4f9270850b
Thanasis Leraj <tleraj@programize.com>
2020-11-02 12:26:27 +0200

    Fix landing page iOS issue

--- packages/website/src/routes/Landing/index.tsx
+++ packages/website/src/routes/Landing/index.tsx
@@ -22,66 +22,72 @@ const LandingPage = ({ classes }: LandingPageProps) => {
   return (
     <>
       <NavBar routeButtons searchLocation={false} />
-      <Box display="flex" alignItems="center" className={classes.landingImage}>
-        <Container className={classes.container}>
-          <Grid container item xs={9}>
-            <Box display="flex">
-              <Typography variant="h1" color="textPrimary">
-                Aqua
-              </Typography>
-              <Typography
-                className={classes.aqualinkSecondPart}
-                color="textPrimary"
-                variant="h1"
-              >
-                link
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid container item sm={11} md={7}>
-            <Box mt="1.5rem" display="flex">
-              <Typography variant="h1" color="textPrimary">
-                Temperature monitoring for marine ecosystems
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid container item sm={9} md={4}>
-            <Box mt="4rem" display="flex">
-              <Typography variant="h4" color="textPrimary">
-                A tool for people on the front line of Ocean conservation
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Box mt="2rem">
-              <Grid container spacing={3}>
-                <Grid item>
-                  <Button
-                    component={Link}
-                    to="/map"
-                    className={classes.buttons}
-                    variant="contained"
-                    color="primary"
-                  >
-                    <Typography variant="h5">View The Map</Typography>
-                  </Button>
+      <Container className={classes.landingImageWrapper}>
+        <Box
+          display="flex"
+          alignItems="center"
+          className={classes.landingImage}
+        >
+          <Container className={classes.container}>
+            <Grid container item xs={9}>
+              <Box display="flex">
+                <Typography variant="h1" color="textPrimary">
+                  Aqua
+                </Typography>
+                <Typography
+                  className={classes.aqualinkSecondPart}
+                  color="textPrimary"
+                  variant="h1"
+                >
+                  link
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid container item sm={11} md={7}>
+              <Box mt="1.5rem" display="flex">
+                <Typography variant="h1" color="textPrimary">
+                  Temperature monitoring for marine ecosystems
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid container item sm={9} md={4}>
+              <Box mt="4rem" display="flex">
+                <Typography variant="h4" color="textPrimary">
+                  A tool for people on the front line of Ocean conservation
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Box mt="2rem">
+                <Grid container spacing={3}>
+                  <Grid item>
+                    <Button
+                      component={Link}
+                      to="/map"
+                      className={classes.buttons}
+                      variant="contained"
+                      color="primary"
+                    >
+                      <Typography variant="h5">View The Map</Typography>
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      component={Link}
+                      to="/register"
+                      className={`${classes.buttons} ${classes.registerButton}`}
+                      variant="outlined"
+                      color="primary"
+                    >
+                      <Typography variant="h5">Register Your Site</Typography>
+                    </Button>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Button
-                    component={Link}
-                    to="/register"
-                    className={`${classes.buttons} ${classes.registerButton}`}
-                    variant="outlined"
-                    color="primary"
-                  >
-                    <Typography variant="h5">Register Your Site</Typography>
-                  </Button>
-                </Grid>
-              </Grid>
-            </Box>
-          </Grid>
-        </Container>
-      </Box>
+              </Box>
+            </Grid>
+          </Container>
+        </Box>
+      </Container>
       <Container>
         {cardTitles.map((item) => (
           <Card
@@ -102,6 +108,11 @@ const LandingPage = ({ classes }: LandingPageProps) => {
 
 const styles = (theme: Theme) =>
   createStyles({
+    landingImageWrapper: {
+      padding: 0,
+      margin: 0,
+      minWidth: "100%",
+    },
     landingImage: {
       backgroundImage: `url("${landingPageImage}")`,
       backgroundSize: "cover",
@@ -119,7 +130,7 @@ const styles = (theme: Theme) =>
     },
     buttons: {
       height: "3rem",
-      width: "12rem",
+      width: "13rem",
       textTransform: "none",
       "&:hover": {
         color: "#ffffff",
