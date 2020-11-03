429b45c7c565087792132574b8fc13f48a5de971
Eric Boucher <eric.p.boucher@gmail.com>
2020-11-03 14:27:11 -0800

    Update index.tsx

--- packages/website/src/common/ErrorPage/index.tsx
+++ packages/website/src/common/ErrorPage/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Typography, Box, Button, useTheme } from "@material-ui/core";
+import { Grid, Typography, Box, Button, useTheme } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 import NavBar from "../NavBar";
 import Footer from "../Footer";
@@ -23,16 +24,28 @@ const ErrorPage = () => {
           alignItems="center"
           justifyContent="center"
         >
-          <Box mb="1rem">
-            <Typography variant="h2">Something went wrong</Typography>
+          <Box mb="1rem" p="1rem">
+            <Typography variant="h2">Sorry, something went wrong...</Typography>
           </Box>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={() => window.location.reload()}
-          >
-            Refresh
-          </Button>
+          <Grid container direction="row" justify="center">
+            <Button
+              style={{ margin: "1rem" }}
+              color="primary"
+              variant="contained"
+              onClick={() => window.location.reload()}
+            >
+              Try Again
+            </Button>
+            <Button
+              style={{ margin: "1rem", color: "white" }}
+              component={Link}
+              to="/map"
+              color="primary"
+              variant="contained"
+            >
+              Back to Map
+            </Button>
+          </Grid>
         </Box>
       </Box>
       <Footer />
