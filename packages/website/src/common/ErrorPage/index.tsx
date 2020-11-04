5bdc5b790b063fed0cb051cb6d50ad57ee51da9e
Ryan <ryan2002.rs@gmail.com>
2020-11-04 19:14:11 +1100

    Simplified Boxes & improve mobile design

--- packages/website/src/common/ErrorPage/index.tsx
+++ packages/website/src/common/ErrorPage/index.tsx
@@ -16,37 +16,33 @@ const ErrorPage = () => {
         height="100%"
         display="flex"
         alignItems="center"
+        flexDirection="column"
         justifyContent="center"
       >
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Box mb="1rem" p="1rem">
-            <Typography variant="h2">Sorry, something went wrong...</Typography>
-          </Box>
-          <Grid container direction="row" justify="center">
-            <Button
-              style={{ margin: "1rem" }}
-              color="primary"
-              variant="contained"
-              onClick={() => window.location.reload()}
-            >
-              Try Again
-            </Button>
-            <Button
-              style={{ margin: "1rem", color: "white" }}
-              component={Link}
-              to="/map"
-              color="primary"
-              variant="contained"
-            >
-              Back to Map
-            </Button>
-          </Grid>
+        <Box mb="1rem" p="1rem">
+          <Typography variant="h2" align="center">
+            Sorry, something went wrong...
+          </Typography>
         </Box>
+        <Grid container direction="row" justify="center">
+          <Button
+            style={{ margin: "1rem" }}
+            color="primary"
+            variant="contained"
+            onClick={() => window.location.reload()}
+          >
+            Try Again
+          </Button>
+          <Button
+            style={{ margin: "1rem", color: "white" }}
+            component={Link}
+            to="/map"
+            color="primary"
+            variant="contained"
+          >
+            Back to Map
+          </Button>
+        </Grid>
       </Box>
       <Footer />
     </>
