d58ed2a8ac141906769f60995b19fcdf3cab605d
tleraj <tleraj@programize.com>
2020-11-04 19:13:48 +0200

    Add error boundary (#351)
    Co-authored-by: Eric Boucher <eric.p.boucher@gmail.com>
    Co-authored-by: Ryan <ryan2002.rs@gmail.com>

---
+++ packages/website/src/layout/App/ErrorBoundary.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component, PropsWithChildren } from "react";
+import ErrorPage from "../../common/ErrorPage";
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<
+  PropsWithChildren<{}>,
+  ErrorBoundaryState
+> {
+  constructor(props: PropsWithChildren<{}>) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError = () => {
+    return { hasError: true };
+  };
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    return hasError ? <ErrorPage /> : children;
+  }
+}
+
+export default ErrorBoundary;
