bed4b7a3c53f1eb930c0ed3e18a420910dce1fdd
Eric Boucher <eric.p.boucher@gmail.com>
2020-11-09 11:28:27 -0800

    Update ReefDetails.tsx

--- packages/website/src/routes/ReefRoutes/Reef/ReefDetails.tsx
+++ packages/website/src/routes/ReefRoutes/Reef/ReefDetails.tsx
@@ -7,6 +7,7 @@ import {
   Theme,
   Box,
   CircularProgress,
+  Typography,
 } from "@material-ui/core";
 import { MaterialUiPickersDate } from "@material-ui/pickers/typings/date";
 import moment from "moment";
@@ -181,8 +182,7 @@ const ReefDetails = ({
                   <CircularProgress size="6rem" thickness={1} />
                 </Box>
               ) : (
-                spotterData &&
-                spotterData.bottomTemperature.length > 1 && (
+                (spotterData && spotterData.bottomTemperature.length > 1 && (
                   <Charts
                     title="HOURLY WATER TEMPERATURE (°C)"
                     dailyData={reef.dailyData}
@@ -196,6 +196,12 @@ const ReefDetails = ({
                     temperatureThreshold={null}
                     background={false}
                   />
+                )) || (
+                  <Box mt="2rem">
+                    <Typography>
+                      No Smart Buoy data available in this time range.
+                    </Typography>
+                  </Box>
                 )
               ))}
             <Surveys reefId={reef.id} />
