a8af385598d7e993a7b6060ddd07518fcb51158b
Thanasis Leraj <tleraj@programize.com>
2020-11-13 19:20:10 +0200

    Remove environment variable

--- packages/website/src/services/mapServices.ts
+++ packages/website/src/services/mapServices.ts
@@ -2,10 +2,8 @@ import axios from "axios";
 
 import type { MapboxGeolocationData } from "../store/Homepage/types";
 
-const {
-  REACT_APP_MAPBOX_BASE_URL: mapboxBaseUrl,
-  REACT_APP_MAPBOX_ACCESS_TOKEN: mapboxToken,
-} = process.env;
+const { REACT_APP_MAPBOX_ACCESS_TOKEN: mapboxToken } = process.env;
+const mapboxBaseUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
 
 const getLocation = (location: string) =>
   axios({
