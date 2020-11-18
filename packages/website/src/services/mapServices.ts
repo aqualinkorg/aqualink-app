72140b748a4aaee25eb499f68fb911b934905828
Eric Boucher <eric.p.boucher@gmail.com>
2020-11-18 13:03:19 -0800

    Add autoselect and geolocation to search bar (#364)
    Co-authored-by: Thanasis Leraj <tleraj@programize.com>

---
+++ packages/website/src/services/mapServices.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+import type { MapboxGeolocationData } from "../store/Homepage/types";
+
+const { REACT_APP_MAPBOX_ACCESS_TOKEN: mapboxToken } = process.env;
+const mapboxBaseUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+
+const getLocation = (location: string) =>
+  axios({
+    method: "GET",
+    url: `${mapboxBaseUrl}${location}.json`,
+    params: {
+      access_token: mapboxToken,
+    },
+  }).then(({ data }) => {
+    const feature = data.features[0];
+    const response: MapboxGeolocationData = {
+      bbox: {
+        southWest: [feature.bbox[1], feature.bbox[0]],
+        northEast: [feature.bbox[3], feature.bbox[2]],
+      },
+      placeName: feature.place_name,
+    };
+    return response;
+  });
+
+export default { getLocation };
