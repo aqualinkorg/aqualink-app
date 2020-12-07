b436bae561f53852bf0c6bbb5736521a90b24a66
Thanas Leraj <tleraj@programize.com>
2020-12-07 11:25:31 +0200

    Add console warning

--- packages/website/src/firebase.ts
+++ packages/website/src/firebase.ts
@@ -3,6 +3,12 @@ import "firebase/auth";
 
 const { REACT_APP_FIREBASE_API_KEY: apiKey } = process.env;
 
+if (!apiKey) {
+  console.warn(
+    "No Firebase API Key provided. Please set up Firebase to use login and admin functionalities."
+  );
+}
+
 const app = apiKey
   ? firebase.initializeApp({
       apiKey,
