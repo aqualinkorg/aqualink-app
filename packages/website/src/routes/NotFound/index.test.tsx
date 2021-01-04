bf8f25f25f1907160347c6b73ebc2fa09b963795
Thanas Leraj <tleraj@programize.com>
2021-01-04 18:43:49 +0200

    Fix tests

--- packages/website/src/routes/NotFound/index.test.tsx
+++ packages/website/src/routes/NotFound/index.test.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Component from ".";
 
 test("renders as expected", () => {
-  const { container } = render(<Component />);
+  const { container } = render(
+    <Router>
+      <Component />
+    </Router>
+  );
   expect(container).toMatchSnapshot();
 });
