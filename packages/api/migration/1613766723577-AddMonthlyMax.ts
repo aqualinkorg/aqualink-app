b6b14072e583f6bccec1634af1df3e4d147e54cb
Eric Boucher <eric.p.boucher@gmail.com>
2021-02-19 14:25:41 -0700

    Add constraint

--- packages/api/migration/1613766723577-AddMonthlyMax.ts
+++ packages/api/migration/1613766723577-AddMonthlyMax.ts
@@ -7,10 +7,10 @@ export class AddMonthlyMax1613766723577 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "monthly_max" ("id" SERIAL NOT NULL, "month" integer NOT NULL, "temperature" double precision NOT NULL, "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "reef_id" integer, CONSTRAINT "PK_14827b8f97d760ba01c95ee4775" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "monthly_max" ADD CONSTRAINT "FK_787e2b4cca2114ef07793eb9be9" FOREIGN KEY ("reef_id") REFERENCES "reef"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`DROP TABLE "monthly_temperature_threshold"`);
+        await queryRunner.query(`ALTER TABLE "monthly_max" ADD CONSTRAINT no_duplicated_month EXCLUDE USING GIST (reef_id, month)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "monthly_max" DROP CONSTRAINT "FK_787e2b4cca2114ef07793eb9be9"`);
         await queryRunner.query(`DROP TABLE "monthly_max"`);
     }
 
