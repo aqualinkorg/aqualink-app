931ff74dced4376c0da95af9703c9338c70a4da3
Antonios Valmas <avalmas@programize.com>
2021-03-01 15:43:32 +0200

    Add hourly query option

--- packages/api/src/time-series/time-series.service.ts
+++ packages/api/src/time-series/time-series.service.ts
@@ -29,26 +29,61 @@ export class TimeSeriesService {
       .merge(this.metricsObject);
   }
 
+  private getDataQuery(
+    startDate: Date,
+    endDate: Date,
+    metrics: Metric[],
+    hourly: boolean,
+    reefId: number,
+    poiId?: number,
+  ) {
+    const poiCondition = poiId ? `poi_id = ${poiId}` : 'poi_id is NULL';
+
+    return hourly
+      ? this.timeSeriesRepository
+          .createQueryBuilder('time_series')
+          .select('avg(value)')
+          .addSelect('metric')
+          .addSelect("date_trunc('hour', timestamp)", 'timestamp')
+          .andWhere('metric IN (:...metrics)', { metrics })
+          .andWhere('reef_id = :reefId', { reefId })
+          .andWhere(poiCondition)
+          .andWhere('timestamp >= :startDate', { startDate })
+          .andWhere('timestamp <= :endDate', { endDate })
+          .groupBy("date_trunc('hour', timestamp), metric")
+          .orderBy("date_trunc('hour', timestamp)", 'ASC')
+          .getRawMany()
+      : this.timeSeriesRepository
+          .createQueryBuilder('time_series')
+          .select('value')
+          .addSelect('metric')
+          .addSelect('timestamp')
+          .andWhere('metric IN (:...metrics)', { metrics })
+          .andWhere('reef_id = :reefId', { reefId })
+          .andWhere(poiCondition)
+          .andWhere('timestamp >= :startDate', { startDate })
+          .andWhere('timestamp <= :endDate', { endDate })
+          .orderBy('timestamp', 'ASC')
+          .getRawMany();
+  }
+
   async findPoiData(
     startDate: Date,
     endDate: Date,
     metrics: Metric[],
+    hourly: boolean,
     poiDataDto: PoiDataDto,
   ) {
     const { reefId, poiId } = poiDataDto;
 
-    const data = await this.timeSeriesRepository
-      .createQueryBuilder('time_series')
-      .select('value')
-      .addSelect('metric')
-      .addSelect('timestamp')
-      .andWhere('metric IN (:...metrics)', { metrics })
-      .andWhere('reef_id = :reefId', { reefId })
-      .andWhere('poi_id = :poiId', { poiId })
-      .andWhere('timestamp >= :startDate', { startDate })
-      .andWhere('timestamp <= :endDate', { endDate })
-      .orderBy('timestamp', 'ASC')
-      .getRawMany();
+    const data = await this.getDataQuery(
+      startDate,
+      endDate,
+      metrics,
+      hourly,
+      reefId,
+      poiId,
+    );
 
     return this.groupByMetric(data);
   }
@@ -57,22 +92,18 @@ export class TimeSeriesService {
     startDate: Date,
     endDate: Date,
     metrics: Metric[],
+    hourly: boolean,
     reefDataDto: ReefDataDto,
   ) {
     const { reefId } = reefDataDto;
 
-    const data = await this.timeSeriesRepository
-      .createQueryBuilder('time_series')
-      .select('value')
-      .addSelect('metric')
-      .addSelect('timestamp')
-      .andWhere('metric IN (:...metrics)', { metrics })
-      .andWhere('reef_id = :reefId', { reefId })
-      .andWhere('poi_id is NULL')
-      .andWhere('timestamp >= :startDate', { startDate })
-      .andWhere('timestamp <= :endDate', { endDate })
-      .orderBy('timestamp', 'ASC')
-      .getRawMany();
+    const data = await this.getDataQuery(
+      startDate,
+      endDate,
+      metrics,
+      hourly,
+      reefId,
+    );
 
     return this.groupByMetric(data);
   }
