0d99f53ccaba3a03c9926a8dbb04f2d49b8e36f4
Eric Boucher <eric.p.boucher@gmail.com>
2021-03-18 15:46:41 -0700

    Add utility script to import historical daily NOAA SST (#486)
    Co-authored-by: Antonios Valmas <avalmas@programize.com>

---
+++ packages/api/scripts/sst-backfill.ts
@@ -0,0 +1,63 @@
+import { createConnection, In } from 'typeorm';
+import Bluebird from 'bluebird';
+import { Point } from 'geojson';
+import { Reef } from '../src/reefs/reefs.entity';
+import { getNOAAData } from './utils/netcdf';
+import { DailyData } from '../src/reefs/daily-data.entity';
+
+const dbConfig = require('../ormconfig');
+
+const palauReefIds = [1632];
+
+async function main() {
+  const connection = await createConnection(dbConfig);
+  const reefRepository = connection.getRepository(Reef);
+  const dailyDataRepository = connection.getRepository(DailyData);
+  const PalauReefs = await reefRepository.find({
+    where: {
+      id: In(palauReefIds),
+    },
+  });
+
+  const dailyDataEntities = PalauReefs.reduce((entities: DailyData[], reef) => {
+    console.log(`Processing reef ${reef.name || reef.id}...`);
+    const allYearEntities = [2017, 2018, 2019, 2020].reduce(
+      (yearEntities: DailyData[], year) => {
+        console.log(`Processing year ${year}...`);
+        const [longitude, latitude] = (reef.polygon as Point).coordinates;
+        const data = getNOAAData(year, longitude, latitude);
+        const yearEntitiesForReef = data.map(
+          ({ date, satelliteTemperature }) =>
+            ({
+              reef: { id: reef.id },
+              date,
+              satelliteTemperature,
+            } as DailyData),
+        );
+        return yearEntities.concat(yearEntitiesForReef);
+      },
+      [],
+    );
+    return entities.concat(allYearEntities);
+  }, []);
+
+  await Bluebird.map(dailyDataEntities, async (entity) => {
+    try {
+      await dailyDataRepository.save(entity);
+    } catch (err) {
+      if (err.constraint === 'no_duplicated_date') {
+        console.debug(
+          `Data already exists for this date ${entity.date.toDateString()}`,
+        );
+      } else {
+        console.error(err);
+      }
+    }
+    dailyDataRepository.create(entity);
+  });
+
+  connection.close();
+  process.exit(0);
+}
+
+main();
--- packages/api/src/utils/coordinates.ts
+++ packages/api/src/utils/coordinates.ts
@@ -14,12 +14,19 @@ export function pointToPixel(
   const geoWidth = Math.abs(maxLong - minLong);
   const geoHeight = Math.abs(maxLat - minLat);
 
-  const tempLong = ((long - minLong) % 360) + minLong;
-  const tempLat = ((lat - minLat) % 180) + minLat;
+  // Normalize longitude and latitude depending on the boundingBox convention
+  const tempLong =
+    minLong >= 0
+      ? (((long % 360) + 540) % 360) - 180
+      : ((long + 180) % 360) - 180;
+
+  const tempLat =
+    minLat >= 0 ? (((lat % 180) + 270) % 180) - 90 : ((lat + 90) % 180) - 90;
 
   // Pixel (0, 0) is the top left corner.
-  const pixelX = Math.round(((tempLong - minLong) / geoWidth) * width);
-  const pixelY = height - Math.round(((tempLat - minLat) / geoHeight) * height);
+  const pixelX = Math.round((Math.abs(tempLong - minLong) / geoWidth) * width);
+  const pixelY =
+    height - Math.round((Math.abs(tempLat - minLat) / geoHeight) * height);
 
   return { pixelX, pixelY };
 }
