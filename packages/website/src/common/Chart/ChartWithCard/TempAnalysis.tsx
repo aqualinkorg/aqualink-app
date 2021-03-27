9099766ef4b2078b0e1ad8ae4133b46ba768c014
Ryan <ryan2002.rs@gmail.com>
2021-03-27 14:14:53 +1100

    Add button and rework how grid works

--- packages/website/src/common/Chart/ChartWithCard/TempAnalysis.tsx
+++ packages/website/src/common/Chart/ChartWithCard/TempAnalysis.tsx
@@ -1,19 +1,18 @@
-import React from "react";
+import React, { FC } from "react";
 import {
   Box,
   Card,
-  Grid,
-  Typography,
-  withStyles,
-  WithStyles,
   createStyles,
+  Grid,
   Theme,
   Tooltip,
+  Typography,
+  WithStyles,
+  withStyles,
 } from "@material-ui/core";
 import moment from "moment";
 import { useSelector } from "react-redux";
 import { isNil } from "lodash";
-
 import { reefTimeSeriesDataLoadingSelector } from "../../../store/Reefs/selectedReefSlice";
 import {
   MonthlyMaxData,
@@ -25,7 +24,8 @@ import { filterMaxMonthlyData } from "../utils";
 import { CardColumn } from "./types";
 import { formatNumber } from "../../../helpers/numberUtils";
 
-const TempAnalysis = ({
+/* eslint-disable react/prop-types */
+const TempAnalysis: FC<TempAnalysisProps> = ({
   pickerStartDate,
   pickerEndDate,
   chartStartDate,
@@ -35,7 +35,8 @@ const TempAnalysis = ({
   hoboBottomTemperature,
   monthlyMax,
   classes,
-}: TempAnalysisProps) => {
+  children,
+}) => {
   const loading = useSelector(reefTimeSeriesDataLoadingSelector);
 
   const filteredMaxMonthlyData = filterMaxMonthlyData(
@@ -96,85 +97,94 @@ const TempAnalysis = ({
   const formattedpickerEndDate = moment(pickerEndDate).format("MM/DD/YYYY");
 
   return (
-    <Box overflow="auto">
-      <Card
-        className={`${classes.tempAnalysisCard} ${
-          hasSpotterData ? classes.scroll : ""
-        }`}
-      >
-        <Typography variant="subtitle1" color="textSecondary">
-          TEMP ANALYSIS
-        </Typography>
-        <Typography className={classes.dates} variant="subtitle2">
-          {formattedpickerStartDate} - {formattedpickerEndDate}
-        </Typography>
-        <Grid
-          className={classes.metricsWrapper}
-          container
-          justify="space-between"
-          alignItems="flex-end"
-          spacing={1}
+    <Box
+      height="100%"
+      display="flex"
+      justifyContent="space-between"
+      flexDirection="column"
+      alignItems="center"
+    >
+      <Box overflow="auto" width="100%">
+        <Card
+          className={`${classes.tempAnalysisCard} ${
+            hasSpotterData ? classes.scroll : ""
+          }`}
         >
-          <Grid item>
-            <Grid
-              className={classes.metrics}
-              container
-              direction="column"
-              item
-              spacing={3}
-            >
-              {rows.map((row) => (
-                <Grid key={row} className={classes.rotatedText} item>
-                  <Typography variant="caption" color="textSecondary">
-                    {row}
-                  </Typography>
-                </Grid>
-              ))}
-            </Grid>
-          </Grid>
-          {cardColumns.map((item) => {
-            if (!isNil(item.rows[0].value)) {
-              return (
-                <Grid key={item.key} item>
-                  <Grid
-                    className={classes.autoWidth}
-                    container
-                    direction="column"
-                    item
-                    spacing={3}
-                    alignItems="center"
-                  >
-                    <Grid item>
-                      <Tooltip title={item.tooltip || ""}>
-                        <Typography
-                          style={{
-                            color: item.color,
-                          }}
-                          variant="subtitle2"
-                        >
-                          {item.title}
-                        </Typography>
-                      </Tooltip>
-                    </Grid>
-                    {item.rows.map(({ key, value }) => (
-                      <Grid key={key} item>
-                        <Typography
-                          className={classes.values}
-                          variant="h5"
-                          color="textSecondary"
-                        >
-                          {formatNumber(value, 1)} °C
-                        </Typography>
-                      </Grid>
-                    ))}
+          <Typography variant="subtitle1" color="textSecondary">
+            TEMP ANALYSIS
+          </Typography>
+          <Typography className={classes.dates} variant="subtitle2">
+            {formattedpickerStartDate} - {formattedpickerEndDate}
+          </Typography>
+          <Grid
+            className={classes.metricsWrapper}
+            container
+            justify="space-between"
+            alignItems="flex-end"
+            spacing={1}
+          >
+            <Grid item>
+              <Grid
+                className={classes.metrics}
+                container
+                direction="column"
+                item
+                spacing={3}
+              >
+                {rows.map((row) => (
+                  <Grid key={row} className={classes.rotatedText} item>
+                    <Typography variant="caption" color="textSecondary">
+                      {row}
+                    </Typography>
                   </Grid>
-                </Grid>
-              );
-            }
-            return null;
-          })}
-        </Grid>
-      </Card>
+                ))}
+              </Grid>
+            </Grid>
+            {cardColumns.map((item) => {
+              if (!isNil(item.rows[0].value)) {
+                return (
+                  <Grid key={item.key} item>
+                    <Grid
+                      className={classes.autoWidth}
+                      container
+                      direction="column"
+                      item
+                      spacing={3}
+                      alignItems="center"
+                    >
+                      <Grid item>
+                        <Tooltip title={item.tooltip || ""}>
+                          <Typography
+                            style={{
+                              color: item.color,
+                            }}
+                            variant="subtitle2"
+                          >
+                            {item.title}
+                          </Typography>
+                        </Tooltip>
+                      </Grid>
+                      {item.rows.map(({ key, value }) => (
+                        <Grid key={key} item>
+                          <Typography
+                            className={classes.values}
+                            variant="h5"
+                            color="textSecondary"
+                          >
+                            {formatNumber(value, 1)} °C
+                          </Typography>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </Grid>
+                );
+              }
+              return null;
+            })}
+          </Grid>
+        </Card>
+      </Box>
+      {children}
     </Box>
   );
 };
@@ -186,10 +196,12 @@ const styles = (theme: Theme) =>
     },
     tempAnalysisCard: {
       padding: 16,
-      height: 250,
+      minHeight: 250,
       borderRadius: "0 4px 4px 0",
       backgroundColor: "#f8f9f9",
-      marginTop: 22,
+      margin: "22px auto",
+      overflow: "auto",
+      maxWidth: 400,
     },
     scroll: {
       [theme.breakpoints.down("xs")]: {
