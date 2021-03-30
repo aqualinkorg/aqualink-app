a0b702507467235a326c3ea68c6062edbbabbabf
Ryan <ryan2002.rs@gmail.com>
2021-03-30 21:42:58 +1100

    Fix performance issues

--- packages/website/src/common/Chart/ChartWithCard/DownloadCSVButton.tsx
+++ packages/website/src/common/Chart/ChartWithCard/DownloadCSVButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { Button } from "@material-ui/core";
 import { CSVLink } from "react-csv";
@@ -8,11 +8,13 @@ import {
 } from "../../../store/Reefs/selectedReefSlice";
 import { DailyData, SofarValue } from "../../../store/Reefs/types";
 
-interface CSVRow {
-  spotterBottomTemp?: number;
-  spotterTopTemp?: number;
-  hoboTemp?: number;
-  dailySST: number;
+type CSVDataColumn =
+  | "spotterBottomTemp"
+  | "spotterTopTemp"
+  | "hoboTemp"
+  | "dailySST";
+
+interface CSVRow extends Partial<Record<CSVDataColumn, number>> {
   timestamp: string;
 }
 
@@ -35,27 +37,31 @@ function constructCSVData({
   hoboData?: SofarValue[];
 }): CSVRow[] {
   const chainedReduce = (
-    finalKey: keyof CSVRow,
+    finalKey: CSVDataColumn,
     data: SofarValue[],
     existingData: Record<CSVRow["timestamp"], CSVRow> = {}
   ) => {
+    // writing this in an immutable fashion will be detrimental to performance.
+    /* eslint-disable no-param-reassign,fp/no-mutation */
     const result = data.reduce((obj, item) => {
-      return {
-        ...obj,
-        [item.timestamp]: {
-          ...obj[item.timestamp],
+      if (obj[item.timestamp]) {
+        obj[item.timestamp][finalKey] = item.value;
+      } else {
+        obj[item.timestamp] = {
           timestamp: item.timestamp,
           [finalKey]: item.value,
-        },
-      };
+        };
+      }
+      return obj;
     }, existingData);
     return {
       result,
       chainedReduce: (
-        chainedFinalKey: keyof CSVRow,
+        chainedFinalKey: CSVDataColumn,
         chainedData: SofarValue[]
       ) => chainedReduce(chainedFinalKey, chainedData, result),
     };
+    /* eslint-enable no-param-reassign,fp/no-mutation */
   };
 
   const mappedToTimestamp = chainedReduce(
@@ -87,16 +93,23 @@ function DownloadCSVButton({
   endDate?: string;
   className?: string;
 }) {
+  console.time("loading data");
   const granularDailyData = useSelector(reefGranularDailyDataSelector);
   const { hobo: hoboData, spotter: spotterData } =
     useSelector(reefTimeSeriesDataSelector) || {};
-  const csvData = constructCSVData({
-    dailyData: granularDailyData,
-    spotterTopTemp: spotterData?.surfaceTemperature,
-    spotterBottomTemp: spotterData?.bottomTemperature,
-    hoboData: hoboData?.bottomTemperature,
-  });
-
+  console.timeEnd("loading data");
+  console.time("csv");
+  const csvData = useMemo(
+    () =>
+      constructCSVData({
+        dailyData: granularDailyData,
+        spotterTopTemp: spotterData?.surfaceTemperature,
+        spotterBottomTemp: spotterData?.bottomTemperature,
+        hoboData: hoboData?.bottomTemperature,
+      }),
+    [granularDailyData, hoboData, spotterData]
+  );
+  console.timeEnd("csv");
   return (
     <CSVLink filename={`export-${startDate}-to-${endDate}.csv`} data={csvData}>
       <Button variant="outlined" color="primary" className={className}>
