c9eb0933e38fc83df89c1bf85498f575c442b275
Eric Boucher <eric.p.boucher@gmail.com>
2021-04-05 16:37:43 -0700

    Frontend tweaks to develop-hobo release (#514)
    Co-authored-by: Thanas Leraj <tleraj@programize.com>
    Co-authored-by: Ryan <ryan2002.rs@gmail.com>

--- packages/website/src/common/Chart/ChartWithCard/DownloadCSVButton.tsx
+++ packages/website/src/common/Chart/ChartWithCard/DownloadCSVButton.tsx
@@ -22,22 +22,28 @@ interface CSVRow extends Partial<Record<CSVDataColumn, number>> {
 const DATE_FORMAT = "YYYY_MM_DD";
 
 /**
- * Construct CSV data to pass into react-csv
+ * Construct CSV data to pass into download-csv.
+ * This function is designed with the 'builder' pattern, where a chain function and result function is returned.
+ * Call the chained() function to add a new column to the CSV, or the result() function to return the final csv object.
+ * @param columnName - The name of the new column. 'timestamp' is reserved.
+ * @param data - The data corresponding to the new column. Obj array of timestamp and value.
+ * @param existingData - Should only used by chained() and never directly. Stores the in-progress CSV object.
  */
 function constructCSVData(
-  finalKey: CSVDataColumn,
+  columnName: CSVDataColumn,
   data: SofarValue[] = [],
   existingData: Record<CSVRow["timestamp"], CSVRow> = {}
 ) {
   // writing this in an immutable fashion will be detrimental to performance.
   /* eslint-disable no-param-reassign,fp/no-mutation,fp/no-mutating-methods */
   const result = data.reduce((obj, item) => {
+    // we are basically ensuring there's always a timestamp column in the final result.
     if (obj[item.timestamp]) {
-      obj[item.timestamp][finalKey] = item.value;
+      obj[item.timestamp][columnName] = item.value;
     } else {
       obj[item.timestamp] = {
         timestamp: item.timestamp,
-        [finalKey]: item.value,
+        [columnName]: item.value,
       };
     }
     return obj;
