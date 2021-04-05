86d0601b1f55218a3aa13b03932252d3583605d8
Eric Boucher <eric.p.boucher@gmail.com>
2021-04-04 17:10:22 -0700

    Fix typos and remove "Palau" reference

--- packages/api/scripts/sst-backfill.ts
+++ packages/api/scripts/sst-backfill.ts
@@ -7,42 +7,45 @@ import { DailyData } from '../src/reefs/daily-data.entity';
 
 const dbConfig = require('../ormconfig');
 
-const selectedReefs = [];
+const reefsToProcess = [];
 
 async function main() {
   const connection = await createConnection(dbConfig);
   const reefRepository = connection.getRepository(Reef);
   const dailyDataRepository = connection.getRepository(DailyData);
-  const PalauReefs = await reefRepository.find({
+  const selectedReefs = await reefRepository.find({
     where:
-      selectedReefs.length > 0
+      reefsToProcess.length > 0
         ? {
-            id: In(selectedReefs),
+            id: In(reefsToProcess),
           }
         : {},
   });
 
-  const dailyDataEntities = PalauReefs.reduce((entities: DailyData[], reef) => {
-    console.log(`Processing reef ${reef.name || reef.id}...`);
-    const allYearEntities = [2017, 2018, 2019, 2020].reduce(
-      (yearEntities: DailyData[], year) => {
-        console.log(`Processing year ${year}...`);
-        const [longitude, latitude] = (reef.polygon as Point).coordinates;
-        const data = getNOAAData(year, longitude, latitude);
-        const yearEntitiesForReef = data.map(
-          ({ date, satelliteTemperature }) =>
-            ({
-              reef: { id: reef.id },
-              date,
-              satelliteTemperature,
-            } as DailyData),
-        );
-        return yearEntities.concat(yearEntitiesForReef);
-      },
-      [],
-    );
-    return entities.concat(allYearEntities);
-  }, []);
+  const dailyDataEntities = selectedReefs.reduce(
+    (entities: DailyData[], reef) => {
+      console.log(`Processing reef ${reef.name || reef.id}...`);
+      const allYearEntities = [2017, 2018, 2019, 2020].reduce(
+        (yearEntities: DailyData[], year) => {
+          console.log(`Processing year ${year}...`);
+          const [longitude, latitude] = (reef.polygon as Point).coordinates;
+          const data = getNOAAData(year, longitude, latitude);
+          const yearEntitiesForReef = data.map(
+            ({ date, satelliteTemperature }) =>
+              ({
+                reef: { id: reef.id },
+                date,
+                satelliteTemperature,
+              } as DailyData),
+          );
+          return yearEntities.concat(yearEntitiesForReef);
+        },
+        [],
+      );
+      return entities.concat(allYearEntities);
+    },
+    [],
+  );
 
   await Bluebird.map(dailyDataEntities, async (entity) => {
     try {
