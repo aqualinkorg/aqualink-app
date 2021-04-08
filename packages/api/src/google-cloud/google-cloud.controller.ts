77539e2dded5acb698886e40ce1b56469f90f8bc
Antonios Valmas <avalmas@programize.com>
2021-04-08 12:05:41 +0300

    Add google cloud and health check docs

--- packages/api/src/google-cloud/google-cloud.controller.ts
+++ packages/api/src/google-cloud/google-cloud.controller.ts
@@ -1,19 +1,27 @@
 import { Controller, Delete, Get } from '@nestjs/common';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { Auth } from '../auth/auth.decorator';
 import { AdminLevel } from '../users/users.entity';
 import { GoogleCloudService } from './google-cloud.service';
 
+@ApiTags('Google Cloud Storage')
 @Auth(AdminLevel.SuperAdmin)
 @Controller('google-cloud')
 export class GoogleCloudController {
   constructor(private googleCloudService: GoogleCloudService) {}
 
-  @Get()
+  @ApiBearerAuth()
+  @ApiOkResponse({
+    description: 'An array of all dangling files',
+    type: [String],
+  })
+  @Get('dangling')
   findDanglingFiles(): Promise<string[]> {
     return this.googleCloudService.findDanglingFiles();
   }
 
-  @Delete()
+  @ApiBearerAuth()
+  @Delete('dangling')
   DeleteDanglingFiles(): Promise<void[]> {
     return this.googleCloudService.deleteDanglingFiles();
   }
--- packages/api/src/health-check/health-check.controller.ts
+++ packages/api/src/health-check/health-check.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Health Check')
 @Controller('health-check')
 export class HealthCheckController {
   @Get()
