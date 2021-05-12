c48caf73065cf873673bde8bd6705910902e3be0
Thanas Leraj <tleraj@programize.com>
2021-05-12 20:43:11 +0300

    Heat Wave Tracker Page (#548)
    Co-authored-by: Eric Boucher <eric.p.boucher@gmail.com>

---
+++ packages/website/src/routes/Tracker/index.test.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { render } from "@testing-library/react";
+import configureStore from "redux-mock-store";
+import { BrowserRouter as Router } from "react-router-dom";
+import { mockUser } from "../../mocks/mockUser";
+
+import Tracker from ".";
+
+jest.mock("../../common/NavBar", () => "Mock-NavBar");
+jest.mock("../../common/Footer", () => "Mock-Footer");
+
+const mockStore = configureStore([]);
+describe("Tracker", () => {
+  let element: HTMLElement;
+  beforeEach(() => {
+    const store = mockStore({
+      user: {
+        userInfo: mockUser,
+        loading: false,
+        error: null,
+      },
+    });
+
+    store.dispatch = jest.fn();
+
+    element = render(
+      <Provider store={store}>
+        <Router>
+          <Tracker />
+        </Router>
+      </Provider>
+    ).container;
+  });
+
+  it("should render with given state from Redux store", () => {
+    expect(element).toMatchSnapshot();
+  });
+});
