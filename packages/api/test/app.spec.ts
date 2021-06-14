4eeb6e563d659bfa1644dce3d82ae2ac7af78781
Antonios Valmas <avalmas@programize.com>
2021-06-14 15:23:39 +0300

    Change timeout and make sure app is defined

--- packages/api/test/app.spec.ts
+++ packages/api/test/app.spec.ts
@@ -1,13 +1,19 @@
+import { INestApplication } from '@nestjs/common';
 import { TestService } from './test.service';
 import { healthCheckTests } from '../src/health-check/health-check.spec';
 import { userTests } from '../src/users/users.spec';
 
 describe('AppController (e2e)', () => {
   const testService = TestService.getInstance();
+  let app: INestApplication;
 
   beforeAll(async () => {
     // Initialize app
-    await testService.getApp();
+    app = await testService.getApp();
+  });
+
+  it('App is defined', () => {
+    expect(app).toBeDefined();
   });
 
   describe('HealthCheck (e2e)', healthCheckTests);
