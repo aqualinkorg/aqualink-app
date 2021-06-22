05f9620c19818bc1a3900e255f964f04844e7d8c
Antonios Valmas <avalmas@programize.com>
2021-06-22 18:22:52 +0300

    Add reef poi tests

--- packages/api/test/app.spec.ts
+++ packages/api/test/app.spec.ts
@@ -5,6 +5,7 @@ import { userTests } from '../src/users/users.spec';
 import { collectionTests } from '../src/collections/collections.spec';
 import { timeSeriesTests } from '../src/time-series/time-series.spec';
 import { regionTests } from '../src/regions/regions.spec';
+import { poiTests } from '../src/reef-pois/reef-pois.spec';
 
 describe('AppController (e2e)', () => {
   const testService = TestService.getInstance();
@@ -22,6 +23,8 @@ describe('AppController (e2e)', () => {
   describe('HealthCheck (e2e) /health-check', healthCheckTests);
 
   describe('User (e2e) /users', userTests);
+
+  describe('Reef Pois (e2e) /pois', poiTests);
 
   describe('Collection (e2e) /collections', collectionTests);
 
