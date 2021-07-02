f2c643fda1db9d838a56ca1bc03ce30181f7d492
Eric Boucher <eric.p.boucher@gmail.com>
2021-07-02 15:24:52 +0200

    Update sstTimeSeries.ts

--- packages/api/src/workers/sstTimeSeries.ts
+++ packages/api/src/workers/sstTimeSeries.ts
@@ -5,7 +5,7 @@ import { TimeSeries } from '../time-series/time-series.entity';
 import { updateSST } from '../utils/sst-time-series';
 
 export function runSSTTimeSeriesUpdate(connection: Connection) {
-  return updateSST([], 3, connection, {
+  return updateSST([], 4, connection, {
     reefRepository: connection.getRepository(Reef),
     timeSeriesRepository: connection.getRepository(TimeSeries),
     sourceRepository: connection.getRepository(Sources),
