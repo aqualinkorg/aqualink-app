a64e63b3db3403b7e4e270e9b386afa526f0bf4a
Thanas Leraj <tleraj@programize.com>
2021-07-06 13:21:00 +0300

    Drop possible query params from video stream url

--- packages/website/src/helpers/video.ts
+++ packages/website/src/helpers/video.ts
@@ -1,6 +1,7 @@
 import { last } from "lodash";
 
-export const getYouTubeVideoId = (url?: string | null) => last(url?.split("/"));
+export const getYouTubeVideoId = (url?: string | null) =>
+  last(url?.split("?")?.[0]?.split("/"));
 
 export const convertOptionsToQueryParams = (options: Record<string, number>) =>
   Object.entries(options).reduce(
