301368b54b5bf55b60d49d3aa5d821a2528c38a2
Antonios Valmas <avalmas@programize.com>
2021-08-05 16:17:30 +0300

    Drop old index

--- packages/api/migration/1627912428897-UpdateSourceConstraint.ts
+++ packages/api/migration/1627912428897-UpdateSourceConstraint.ts
@@ -7,6 +7,7 @@ export class UpdateSourceConstraint1627912428897 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "sources" DROP CONSTRAINT "no_duplicate_sources"`,
     );
+    await queryRunner.query(`DROP INDEX "IDX_84204417a187000377a03dc532"`);
     await queryRunner.query(
       `CREATE UNIQUE INDEX "no_duplicate_sources" ON "sources" ("reef_id", COALESCE("poi_id", 0), "type", COALESCE("sensor_id", 'SPOT-IMPOSSIBLE'))`,
     );
@@ -16,6 +17,9 @@ export class UpdateSourceConstraint1627912428897 implements MigrationInterface {
     await queryRunner.query(`DROP INDEX "no_duplicate_sources"`);
     await queryRunner.query(
       `ALTER TABLE "sources" ADD CONSTRAINT "no_duplicate_sources" UNIQUE ("reef_id", "poi_id", "type")`,
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_84204417a187000377a03dc532" ON "sources" ("reef_id", "type") WHERE "poi_id" IS NULL`,
     );
   }
 }
