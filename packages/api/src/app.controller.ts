a661446c1ce15b9138d95fe758ac4e82e0d54b2b
avalmas-programize <69854742+avalmas-programize@users.noreply.github.com>
2021-08-07 11:19:14 +0300

    Ignore redirect endpoint on api docs (#616)
    Co-authored-by: Eric Boucher <eric.p.boucher@gmail.com>

--- packages/api/src/app.controller.ts
+++ packages/api/src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Logger, Redirect } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller('')
 export class AppController {
   private readonly logger = new Logger(AppController.name);
 
+  @ApiExcludeEndpoint()
   @Get()
   @Redirect('/api/docs')
   getDocs() {
