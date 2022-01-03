84123e780708c8cb9b09d0344b6581bbcfa037e7
Thanas Leraj <tleraj@programize.com>
2022-01-03 15:57:42 +0200

    Create hook to fetch site details when necessary

--- packages/website/src/routes/SiteRoutes/UploadData/index.tsx
+++ packages/website/src/routes/SiteRoutes/UploadData/index.tsx
@@ -1,5 +1,21 @@
+import { LinearProgress } from "@material-ui/core";
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
 
-const UploadData = () => <span>Upload sonde data</span>;
+import NavBar from "../../../common/NavBar";
+import { useSiteRequest } from "../../../hooks/useSiteRequest";
+
+const UploadData = ({ match }: MatchProps) => {
+  const { siteLoading } = useSiteRequest(match.params.id);
+
+  return (
+    <>
+      <NavBar searchLocation={false} />
+      {siteLoading && <LinearProgress />}
+    </>
+  );
+};
+
+interface MatchProps extends RouteComponentProps<{ id: string }> {}
 
 export default UploadData;
