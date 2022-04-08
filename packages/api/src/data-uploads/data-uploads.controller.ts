f3e11046cfdf1da8cc37e58742783b2fe9949bd4
echaidemenos <echaidemenos@programize.com>
2022-04-08 13:32:32 +0300

    Add delete multiple data upload files endpoint

--- packages/api/src/data-uploads/data-uploads.controller.ts
+++ packages/api/src/data-uploads/data-uploads.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { SiteDataRangeDto } from '../time-series/dto/site-data-range.dto';
 import { DataUploadsService } from './data-uploads.service';
+import { DataUploadsDeleteDto } from './dto/data-uploads-delete.dto';
 
 @ApiTags('Data Uploads')
 @Controller('data-uploads')
@@ -12,5 +13,11 @@ export class DataUploadsController {
   @Get('sites/:siteId')
   getDataUploads(@Param() params: SiteDataRangeDto) {
     return this.dataUploadsService.getDataUploads(params);
+  }
+
+  @ApiOperation({ summary: 'Delete selected data uploads' })
+  @Post('delete-uploads')
+  DeleteDataUploads(@Body() dataUploadsDeleteDto: DataUploadsDeleteDto) {
+    return this.dataUploadsService.deleteDataUploads(dataUploadsDeleteDto);
   }
 }
