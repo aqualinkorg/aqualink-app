05ecf995c2d45868da8a1cd8bea4ddc819ef8b74
echaidemenos <echaidemenos@programize.com>
2022-05-06 18:34:31 +0300

    Remove unneeded imports

--- packages/api/src/time-series/time-series.module.ts
+++ packages/api/src/time-series/time-series.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataUploads } from '../data-uploads/data-uploads.entity';
-import { Region } from '../regions/regions.entity';
 import { SiteSurveyPoint } from '../site-survey-points/site-survey-points.entity';
-import { HistoricalMonthlyMean } from '../sites/historical-monthly-mean.entity';
 import { Site } from '../sites/sites.entity';
 import { Sources } from '../sites/sources.entity';
-import { SurveyMedia } from '../surveys/survey-media.entity';
-import { Survey } from '../surveys/surveys.entity';
-import { User } from '../users/users.entity';
 import { EntityExists } from '../validations/entity-exists.constraint';
 import { TimeSeriesController } from './time-series.controller';
 import { TimeSeries } from './time-series.entity';
@@ -24,11 +19,6 @@ import { TimeSeriesService } from './time-series.service';
       SiteSurveyPoint,
       Sources,
       DataUploads,
-      User,
-      Survey,
-      SurveyMedia,
-      Region,
-      HistoricalMonthlyMean,
     ]),
   ],
 })
