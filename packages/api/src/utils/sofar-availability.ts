60b08e7fff8b704ee4d4f6ecedc6dc48b94b7fe8
echaidemenos <echaidemenos@programize.com>
2022-06-03 15:57:12 +0300

    Revert "Fix lint issue"
    This reverts commit 97831a224ee664f7ddebd2ec461158bf82c3b34e.

--- packages/api/src/utils/sofar-availability.ts
+++ packages/api/src/utils/sofar-availability.ts
@@ -3,19 +3,25 @@ import type { FeatureCollection, Point } from 'geojson';
 import nearestPoint from '@turf/nearest-point';
 import availabilityPoints from './sofar-availability-points';
 
-const geojson: FeatureCollection<Point> = {
-  type: 'FeatureCollection',
-  features: availabilityPoints.map((coordinate) => ({
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: coordinate,
-    },
-  })),
-};
+let geojson;
+export function getSofarWaveModelAvailability(): FeatureCollection<Point> {
+  if (!geojson) {
+    geojson = {
+      type: 'FeatureCollection',
+      features: availabilityPoints.map((coordinate) => ({
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: coordinate,
+        },
+      })),
+    };
+  }
+  return geojson;
+}
 
 export function getSofarNearestAvailablePoint(point: Coord): [number, number] {
-  const availablePoints = geojson;
+  const availablePoints = getSofarWaveModelAvailability();
   // deconstructing number[] into [number, number] in order to make typescript compiler happy
   const [longitude, latitude] = nearestPoint(point, availablePoints).geometry
     .coordinates;
