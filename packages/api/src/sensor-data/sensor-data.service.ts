e5c3ea0d3e5a66d07f0babac7add4e2128a6c598
Denys Pysarenko <xxxd0nixxx1993@gmail.com>
2022-07-05 15:05:07 +0300

    Fix argument positions

--- packages/api/src/sensor-data/sensor-data.service.ts
+++ packages/api/src/sensor-data/sensor-data.service.ts
@@ -3,7 +3,7 @@ import { sofarSensor } from '../utils/sofar';
 
 @Injectable()
 export class SensorDataService {
-  get(sensorId: string, endDate?: string, startDate?: string) {
+  get(sensorId: string, startDate?: string, endDate?: string) {
     return sofarSensor(sensorId, startDate, endDate);
   }
 }
