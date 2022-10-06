74d5cd0d19c65c96693be314b718168e2cc369b8
Eleftherios Chaidemenos <echaidemenos@programize.com>
2022-10-06 13:39:25 +0300

    Resolve PR comments

--- packages/website/src/utils/google-analytics.ts
+++ packages/website/src/utils/google-analytics.ts
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactGA from "react-ga";
 
 const GA_TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID || "";
@@ -8,6 +9,49 @@ if (process.env.IS_PROD && !GA_TRACKING_ID) {
       " .env.prod file."
   );
 }
+
+const googleAnalyticsTagManagerID =
+  process.env.REACT_APP_GA_TAG_MANAGER_ID || "";
+if (process.env.IS_PROD && !GA_TRACKING_ID) {
+  throw new Error(
+    "You appear to be trying to do a production build, but no Google Analytics" +
+      " tag manager id was provided!\nEither set REACT_APP_GA_TAG_MANAGER_ID as an env variable, or set up a" +
+      " .env.prod file."
+  );
+}
+
+export const useGATagManager = () => {
+  const getScript = () => `
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({"gtm.start": new Date().getTime(), event:"gtm.js"});
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != "dataLayer" ? "&l=" + l : "";
+      j.async = true;
+      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+      f.parentNode.insertBefore(j,f);
+    })(
+      window,
+      document,
+      "script",
+      "dataLayer",
+      "${googleAnalyticsTagManagerID}",
+    );
+  `;
+
+  React.useEffect(() => {
+    const script = document.createElement("script");
+    // eslint-disable-next-line fp/no-mutation
+    script.innerHTML = getScript();
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [googleAnalyticsTagManagerID]);
+};
 
 export const initGA = () => {
   ReactGA.initialize(GA_TRACKING_ID);
