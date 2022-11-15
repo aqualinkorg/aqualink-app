9744979f2a359a2d1e505373ba57abc9084efb39
Eleftherios Chaidemenos <echaidemenos@programize.com>
2022-11-15 17:01:00 +0200

    Add site creation and data uploading logic. Add logging

--- packages/api/scripts/upload-hui-data.ts
+++ packages/api/scripts/upload-hui-data.ts
@@ -1,13 +1,18 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import yargs from 'yargs';
 import { Logger } from '@nestjs/common';
-import { ConnectionOptions, createConnection } from 'typeorm';
+import { ConnectionOptions, createConnection, Repository } from 'typeorm';
 import { get, groupBy, last, maxBy, minBy } from 'lodash';
 import clustersDbscan from '@turf/clusters-dbscan';
+import Bluebird from 'bluebird';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 import { configService } from '../src/config/config.service';
 import {
   convertData,
+  findOrCreateSourceEntity,
   getFilePathData,
+  refreshMaterializedView,
+  saveBatchToTimeSeries,
   uploadFileToGCloud,
 } from '../src/utils/uploads/upload-sheet-data';
 import { SourceType } from '../src/sites/schemas/source-type.enum';
@@ -15,6 +20,10 @@ import { Sources } from '../src/sites/sources.entity';
 import { Site } from '../src/sites/sites.entity';
 import { SiteSurveyPoint } from '../src/site-survey-points/site-survey-points.entity';
 import { DataUploads } from '../src/data-uploads/data-uploads.entity';
+import { createPoint } from '../src/utils/coordinates';
+import { Region } from '../src/regions/regions.entity';
+import { getRegion, getTimezones } from '../src/utils/site.utils';
+import { TimeSeries } from '../src/time-series/time-series.entity';
 
 const { argv } = yargs
   .scriptName('upload-hui-data')
@@ -31,6 +40,82 @@ const { argv } = yargs
   })
   .help();
 
+interface NewData {
+  timestamp: string;
+  value: number;
+  metric: string;
+  source: Sources;
+}
+
+interface PointInfo {
+  cluster: number | undefined;
+  name: string;
+  site: Site | undefined;
+  siteSurveyPoint: SiteSurveyPoint | undefined;
+  coordinates: [number, number];
+  data: NewData[];
+}
+
+const sitesCreated: Site[] = [];
+const siteSurveyPointsCreated: SiteSurveyPoint[] = [];
+
+function createSourceAndInsertTimeSeries(
+  site: Site,
+  points: PointInfo[],
+  dataUploadsFile: Awaited<ReturnType<typeof uploadFileToGCloud>> | undefined,
+  siteSurveyPointRepository: Repository<SiteSurveyPoint>,
+  sourcesRepository: Repository<Sources>,
+  timeSeriesRepository: Repository<TimeSeries>,
+) {
+  return points.map(async (point) => {
+    let targetSite: Site;
+    let targetSiteSurveyPoint: SiteSurveyPoint | null;
+
+    if (point.site !== undefined) {
+      // eslint-disable-next-line fp/no-mutation
+      targetSite = point.site;
+      // eslint-disable-next-line fp/no-mutation
+      targetSiteSurveyPoint = null;
+    } else if (point.siteSurveyPoint !== undefined) {
+      // eslint-disable-next-line fp/no-mutation
+      targetSite = point.siteSurveyPoint.site;
+      // eslint-disable-next-line fp/no-mutation
+      targetSiteSurveyPoint = point.siteSurveyPoint;
+    } else {
+      // eslint-disable-next-line fp/no-mutation
+      targetSite = site;
+      // eslint-disable-next-line fp/no-mutation
+      targetSiteSurveyPoint = await siteSurveyPointRepository.save({
+        name: point.name,
+        polygon: createPoint(point.coordinates[1], point.coordinates[0]),
+        site,
+      });
+      // eslint-disable-next-line fp/no-mutating-methods
+      siteSurveyPointsCreated.push(targetSiteSurveyPoint);
+    }
+
+    const sourceEntity = await findOrCreateSourceEntity(
+      targetSite,
+      SourceType.HUI,
+      targetSiteSurveyPoint,
+      sourcesRepository,
+    );
+    const dataAsTimeSeries = point.data.map((x) => {
+      return {
+        timestamp: x.timestamp,
+        value: x.value,
+        metric: x.metric,
+        source: sourceEntity,
+        dataUpload: dataUploadsFile,
+      };
+    });
+    return saveBatchToTimeSeries(
+      dataAsTimeSeries as QueryDeepPartialEntity<TimeSeries>[],
+      timeSeriesRepository,
+    );
+  });
+}
+
 async function run() {
   // Initialize Nest logger
   const logger = new Logger('ParseHUIData');
@@ -43,7 +128,11 @@ async function run() {
   const siteRepository = connection.getRepository(Site);
   const siteSurveyPointRepository = connection.getRepository(SiteSurveyPoint);
   const dataUploadsRepository = connection.getRepository(DataUploads);
+  const regionRepository = connection.getRepository(Region);
   const sourcesRepository = connection.getRepository(Sources);
+  const timeSeriesRepository = connection.getRepository(TimeSeries);
+
+  logger.log(`Processing data from file: ${filePath}`);
 
   const {
     workSheetData,
@@ -65,7 +154,7 @@ async function run() {
       const site = await siteRepository.findOne({ where: { name: key } });
       const siteSurveyPoint = site
         ? undefined
-        : siteSurveyPointRepository.findOne({ where: { name: key } });
+        : await siteSurveyPointRepository.findOne({ where: { name: key } });
       return {
         name: key,
         site,
@@ -83,7 +172,7 @@ async function run() {
           ignoredHeaders,
           '',
           {} as Sources,
-        ),
+        ) as NewData[],
       };
     }),
   );
@@ -97,24 +186,28 @@ async function run() {
     properties: {},
   }));
 
-  const clusterd = clustersDbscan(
+  const clustered = clustersDbscan(
     {
       type: 'FeatureCollection',
       features: sitesAsFeatureCollection,
     },
-    1,
+    2,
     {
       units: 'kilometers',
       minPoints: 1,
     },
   );
 
-  const sitesClusterd = sitesWithData.map((x, i) => ({
+  const sitesClustered = sitesWithData.map((x, i) => ({
     ...x,
-    cluster: clusterd.features[i].properties.cluster,
+    cluster: clustered.features[i].properties.cluster,
   }));
 
-  const allData = sitesClusterd.reduce(
+  logger.log(`Found ${sitesClustered.length} sites.`);
+  const clusterNum = sitesClustered[sitesClustered.length - 1].cluster;
+  if (clusterNum) logger.log(`Grouped sites into ${clusterNum + 1} clusters`);
+
+  const allData = sitesClustered.reduce(
     (acc, curr) => {
       return [...acc, ...curr.data];
     },
@@ -135,24 +228,81 @@ async function run() {
     'timestamp',
   );
 
-  const dataUploadsFile = false
-    ? await uploadFileToGCloud(
-        dataUploadsRepository,
-        signature,
-        undefined,
-        undefined,
-        SourceType.HUI,
-        last(filePath.split('/')) || '',
-        filePath,
-        minDate,
-        maxDate,
-        importedHeaders,
-      )
-    : undefined;
+  const dataUploadsFile = await uploadFileToGCloud(
+    dataUploadsRepository,
+    signature,
+    undefined,
+    undefined,
+    SourceType.HUI,
+    last(filePath.split('/')) || '',
+    filePath,
+    minDate,
+    maxDate,
+    importedHeaders,
+  );
 
-  // Do the actual site, site survey point creation and time series upload
+  const groupedClusteredSites = groupBy(sitesClustered, (x) => x.cluster);
 
-  logger.log('Uploading hobo data');
+  const promises = Object.keys(groupedClusteredSites).map(async (key) => {
+    const pointsList = groupedClusteredSites[key];
+    const siteIndex = pointsList.findIndex((x) => x.site !== undefined);
+    const mainSite = siteIndex !== -1 ? pointsList[siteIndex] : pointsList[0];
+    const timezones = getTimezones(
+      mainSite.coordinates[0],
+      mainSite.coordinates[1],
+    ) as string[];
+    const region = await getRegion(
+      mainSite.coordinates[0],
+      mainSite.coordinates[1],
+      regionRepository,
+    );
+    const site = mainSite.site
+      ? mainSite.site
+      : await siteRepository.save({
+          name: mainSite.name,
+          region,
+          polygon: createPoint(
+            mainSite.coordinates[1],
+            mainSite.coordinates[0],
+          ),
+          timezone: timezones[0],
+          display: false,
+        });
+
+    if (mainSite.site === undefined) {
+      // eslint-disable-next-line fp/no-mutating-methods
+      sitesCreated.push(site);
+      // eslint-disable-next-line fp/no-mutation
+      mainSite.site = site;
+    }
+    await Bluebird.Promise.all(
+      createSourceAndInsertTimeSeries(
+        site,
+        pointsList,
+        dataUploadsFile,
+        siteSurveyPointRepository,
+        sourcesRepository,
+        timeSeriesRepository,
+      ),
+    );
+    return key;
+  });
+
+  await Bluebird.Promise.each(promises, (clusterId) => {
+    logger.log(`Completed cluster ${clusterId}`);
+  });
+
+  refreshMaterializedView(dataUploadsRepository);
+
+  logger.warn('The following sites where created:');
+  logger.log(
+    sitesCreated.map((x) => ({
+      id: x.id,
+      name: x.name,
+    })),
+  );
+  logger.warn(`The following site's survey points where created:`);
+  logger.log(siteSurveyPointsCreated.map((x) => ({ id: x.id, name: x.name })));
 }
 
 run();
