0ca22bf846a17e1235f825c5b94c20b00418cb36
echaidemenos <80451946+echaidemenos@users.noreply.github.com>
2023-06-12 18:05:17 +0300

    Remove webpack for functions deployment (#866)
    * Remove webpack for functions deployment
    
    * Fix functions would not get config
    
    * App would not start in docker
    
    * Re add per-prossesing script
    
    * Add firebase-admin to deps
    
    * Update ONBOARDING.md

--- packages/api/src/sensor-data/sensor-data.service.ts
+++ packages/api/src/sensor-data/sensor-data.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { SOFAR_API_TOKEN } from '../utils/constants';
 import { sofarSensor } from '../utils/sofar';
 
 @Injectable()
 export class SensorDataService {
   get(sensorId: string, startDate?: string, endDate?: string) {
-    return sofarSensor(sensorId, SOFAR_API_TOKEN, startDate, endDate);
+    return sofarSensor(
+      sensorId,
+      process.env.SOFAR_API_TOKEN,
+      startDate,
+      endDate,
+    );
   }
 }
