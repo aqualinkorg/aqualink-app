4c35df756a7fcdf1f71842978d5b8eba5f782463
Eleftherios Chaidemenos <echaidemenos@programize.com>
2023-07-06 12:06:11 +0300

    Improve downloadCsvFile function

--- packages/website/src/utils/utils.ts
+++ packages/website/src/utils/utils.ts
@@ -1,22 +1,10 @@
 export function downloadCsvFile(url: string, fileName: string) {
-  return fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'text/csv',
-    },
-  })
-    .then((response) => {
-      return response.blob();
-    })
-    .then((blob) => {
-      const downloadUrl = window.URL.createObjectURL(new Blob([blob]));
-      const link = document.createElement('a');
-      // eslint-disable-next-line fp/no-mutation
-      link.href = downloadUrl;
-      if (fileName) {
-        link.setAttribute('download', fileName);
-      }
-      document.body.appendChild(link);
-      link.click();
-    });
+  const link = document.createElement('a');
+  // eslint-disable-next-line fp/no-mutation
+  link.href = url;
+  if (fileName) {
+    link.setAttribute('download', fileName);
+  }
+  document.body.appendChild(link);
+  link.click();
 }
