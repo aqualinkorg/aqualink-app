755ec3b0679ab0c46d11f39b8bc2f7aafff2e852
echaidemenos <80451946+echaidemenos@users.noreply.github.com>
2023-07-20 12:21:00 +0300

    Change timout to 1min

--- packages/api/migration/1689588980336-AddQueryTimeout.ts
+++ packages/api/migration/1689588980336-AddQueryTimeout.ts
@@ -4,7 +4,7 @@ export class AddQueryTimeout1689588980336 implements MigrationInterface {
   name = 'AddQueryTimeout1689588980336';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('SET statement_timeout = 40000;');
+    await queryRunner.query('SET statement_timeout = 60000;');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
