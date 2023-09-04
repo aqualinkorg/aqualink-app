9903f55d656bf6be4d928edf27aca3e14b1deb6d
Eleftherios Chaidemenos <echaidemenos@programize.com>
2023-09-04 18:52:15 +0300

    Fix styling issue

--- packages/api/migration/1693562630962-AddScheduledUpdatesTable.ts
+++ packages/api/migration/1693562630962-AddScheduledUpdatesTable.ts
@@ -10,7 +10,7 @@ export class AddScheduledUpdatesTable1693562630962
       `CREATE TABLE "scheduled_update" ("id" SERIAL NOT NULL, "site_id" integer NOT NULL, CONSTRAINT "PK_192e2322a4b9c2da520537a2a46" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `CREATE INDEX "IDX_658dfd2b982a40e1928c46fbab" ON "scheduled_update" ("site_id") `,
+      `CREATE INDEX "IDX_658dfd2b982a40e1928c46fbab" ON "scheduled_update" ("site_id")`,
     );
     await queryRunner.query(
       `ALTER TABLE "scheduled_update" ADD CONSTRAINT "FK_658dfd2b982a40e1928c46fbab2" FOREIGN KEY ("site_id") REFERENCES "site"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
