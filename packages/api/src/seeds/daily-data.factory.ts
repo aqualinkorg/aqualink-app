febe5e96a6d452817a52fb13e728062fe51c8336
Eleftherios Chaidemenos <echaidemenos@programize.com>
2023-10-03 15:35:24 +0300

    Fix tests

--- packages/api/src/seeds/daily-data.factory.ts
+++ packages/api/src/seeds/daily-data.factory.ts
@@ -7,28 +7,8 @@ define(DailyData, (faker: typeof Faker) => {
   const dailyData = new DailyData();
   dailyData.date = faker.date.past(1);
 
-  const avgBottomTemperature = faker.random.number({ min: 20, max: 40 });
-  dailyData.minBottomTemperature = avgBottomTemperature - 1;
-  dailyData.maxBottomTemperature = avgBottomTemperature + 1;
-  dailyData.avgBottomTemperature = avgBottomTemperature;
-
   dailyData.degreeHeatingDays = faker.random.number({ min: 20, max: 40 });
-  dailyData.topTemperature = faker.random.number({ min: 20, max: 40 });
   dailyData.satelliteTemperature = faker.random.number({ min: 20, max: 40 });
-
-  const avgWaveHeight = faker.random.number({ min: 2, max: 4 });
-  dailyData.minWaveHeight = avgWaveHeight - 1;
-  dailyData.maxWaveHeight = avgWaveHeight + 1;
-  dailyData.avgWaveHeight = avgWaveHeight;
-  dailyData.waveMeanDirection = faker.random.number({ min: 0, max: 360 });
-  dailyData.waveMeanPeriod = faker.random.number({ min: 10, max: 30 });
-
-  const avgWindSpeed = faker.random.number({ min: 2, max: 4 });
-  dailyData.minWindSpeed = avgWindSpeed - 1;
-  dailyData.maxWindSpeed = avgWindSpeed + 1;
-  dailyData.avgWindSpeed = avgWindSpeed;
-  dailyData.windDirection = faker.random.number({ min: 0, max: 360 });
-  dailyData.dailyAlertLevel = faker.random.number({ min: 0, max: 5 });
 
   return dailyData;
 });
