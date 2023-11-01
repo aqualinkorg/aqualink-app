50ba20244ed098e9055459d239fd2e92e8502e26
Eleftherios Chaidemenos <echaidemenos@programize.com>
2023-11-01 13:53:30 +0200

    Add limit for siteIds

--- packages/api/src/monitoring/dto/get-monitoring-stats.dto.ts
+++ packages/api/src/monitoring/dto/get-monitoring-stats.dto.ts
@@ -5,12 +5,21 @@ import { IsDate, IsNumber, IsOptional, Validate } from 'class-validator';
 import { Site } from 'sites/sites.entity';
 import { EntityExists } from 'validations/entity-exists.constraint';
 
+const maxAllowedIds = 10;
+
 export class GetMonitoringStatsDto {
   @ApiProperty({ example: [1, 3, 5] })
   @IsOptional()
   @Transform(({ value }) => {
     try {
       const splitted = value.split(',');
+
+      if (splitted.length > maxAllowedIds) {
+        throw new BadRequestException(
+          `siteIds: Too many IDs. Maximum allowed: ${maxAllowedIds}`,
+        );
+      }
+
       return splitted.map((x) => parseInt(x, 10));
     } catch (error) {
       throw new BadRequestException('siteIds: invalid format');
