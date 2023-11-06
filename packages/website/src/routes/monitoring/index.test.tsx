cb8bae96f3b1eb1860d03773e7b910298980fba5
Eleftherios Chaidemenos <echaidemenos@programize.com>
2023-11-06 12:01:23 +0200

    Update tests

--- packages/website/src/routes/monitoring/index.test.tsx
+++ packages/website/src/routes/monitoring/index.test.tsx
@@ -6,12 +6,29 @@ import configureStore from 'redux-mock-store';
 import { mockUser } from 'mocks/mockUser';
 import { SnackbarProvider } from 'notistack';
 import { mockCollection } from 'mocks/mockCollection';
+import { advanceTo, clear } from 'jest-date-mock';
 import MonitoringPage from '.';
 
 const mockStore = configureStore([]);
 
+jest.mock('@material-ui/pickers', () => ({
+  __esModule: true,
+  KeyboardDatePicker: 'mock-KeyboardDatePicker',
+  KeyboardDatePickerProps: 'mock-KeyboardDatePickerProps',
+  MuiPickersUtilsProvider: 'mock-MuiPickersUtilsProvider',
+}));
+
 describe('Landing Page', () => {
   let element: HTMLElement;
+
+  beforeAll(() => {
+    advanceTo(new Date('2023-10-20T12:00:00'));
+  });
+
+  afterAll(() => {
+    clear();
+  });
+
   beforeEach(() => {
     const store = mockStore({
       user: {
