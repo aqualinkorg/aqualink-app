e75cada0fc4a880905c9f5440867e5399193ba77
Eric Boucher <eric.p.boucher@gmail.com>
2024-04-30 23:47:27 +0200

    Update ChartWithTooltip.tsx

--- packages/website/src/common/Chart/ChartWithTooltip.tsx
+++ packages/website/src/common/Chart/ChartWithTooltip.tsx
@@ -120,7 +120,7 @@ function ChartWithTooltip({
           },
           hover: {
             mode: 'index',
-            intersect: false
+            intersect: false,
           },
           legend: {
             display: false,
