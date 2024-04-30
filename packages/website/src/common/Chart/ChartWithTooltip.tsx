1805a836d59b4a3835bbb9b6697c1f6967b76a90
Eric Boucher <eric.p.boucher@gmail.com>
2024-04-30 23:31:01 +0200

    put tooltip a bit higher

--- packages/website/src/common/Chart/ChartWithTooltip.tsx
+++ packages/website/src/common/Chart/ChartWithTooltip.tsx
@@ -77,7 +77,7 @@ function ChartWithTooltip({
       const top =
         position.top +
         tooltipModel.caretY -
-        ((surveyId ? 30 : 0) + nValues * 20 + 50);
+        ((surveyId ? 30 : 0) + nValues * 20 + 60);
 
       // We display the tooltip only if there are data to display at this point and it lands
       // between the chart's X axis limits.
