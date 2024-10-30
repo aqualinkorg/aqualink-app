2e3bf1f1b5ee8a21b455f06f3ec864589286d7b2
Konstantinos Markopoulos <konst.markopoulos@gmail.com>
2024-10-30 15:05:50 +0200

    Update snapshots and add tests

---
+++ packages/website/src/common/SiteDetails/TemperatureChange/index.test.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import { getMockDailyData } from 'mocks/mockDailyData';
+import { TemperatureChange } from '.';
+
+describe('TemperatureChange card', () => {
+  const mockDailyData = getMockDailyData(14);
+
+  function renderTemperatureChange(dailyData = mockDailyData) {
+    return render(
+      <Router>
+        <TemperatureChange dailyData={dailyData} />,
+      </Router>,
+    );
+  }
+
+  it('should render with given state from Redux store', () => {
+    const { container } = renderTemperatureChange();
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render the right temperature change', () => {
+    const satelliteTemp = [
+      22, 23, 23, 25, 26, 25, 22, 24, 20, 19, 22, 26, 24, 23,
+    ];
+    const dailyData = getMockDailyData(14).map((data, i) => ({
+      ...data,
+      satelliteTemperature: satelliteTemp[i],
+    }));
+    const { container } = renderTemperatureChange(dailyData);
+
+    const temperatureChange = container.querySelector('[variant="h1"]');
+
+    expect(temperatureChange).toHaveTextContent('+1.1°C');
+  });
+
+  it('should render -- when there is no data', () => {
+    const { container } = renderTemperatureChange([]);
+    const temperatureChange = container.querySelector('[variant="h1"]');
+
+    expect(temperatureChange).toHaveTextContent('--°C');
+  });
+
+  it('should render -- when there is no data for both weeks', () => {
+    const { container } = renderTemperatureChange(mockDailyData.slice(0, 7));
+    const temperatureChange = container.querySelector('[variant="h1"]');
+
+    expect(temperatureChange).toHaveTextContent('--°C');
+  });
+});
