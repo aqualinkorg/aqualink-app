c6174f52a15ce5c9aa550f845583e91a379ccba9
Konstantinos Markopoulos <konst.markopoulos@gmail.com>
2024-12-02 15:32:53 +0200

    Encode url params

--- packages/website/src/services/reefCheckService.ts
+++ packages/website/src/services/reefCheckService.ts
@@ -3,6 +3,8 @@ import { ReefCheckSurvey } from 'store/ReefCheckSurveys/types';
 
 export const getReefCheckSurvey = async (siteId: string, id: string) =>
   requests.send<ReefCheckSurvey>({
-    url: `reef-check-sites/${siteId}/surveys/${id}`,
+    url: `reef-check-sites/${encodeURIComponent(
+      siteId,
+    )}/surveys/${encodeURIComponent(id)}`,
     method: 'GET',
   });
