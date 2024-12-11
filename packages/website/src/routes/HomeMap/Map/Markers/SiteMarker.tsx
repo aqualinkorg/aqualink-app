3a84bf75a3101b350ec53af9e2e9237eb451c29f
Konstantinos Markopoulos <konst.markopoulos@gmail.com>
2024-12-11 17:39:47 +0200

    Memoize sitemarker

--- packages/website/src/routes/HomeMap/Map/Markers/SiteMarker.tsx
+++ packages/website/src/routes/HomeMap/Map/Markers/SiteMarker.tsx
@@ -1,11 +1,11 @@
-import { Marker, useLeaflet } from 'react-leaflet';
+import { Marker } from 'react-leaflet';
 import { useDispatch, useSelector } from 'react-redux';
 import React from 'react';
 import { Site } from 'store/Sites/types';
 import {
-  siteOnMapSelector,
   setSiteOnMap,
   setSearchResult,
+  isSelectedOnMapSelector,
 } from 'store/Homepage/homepageSlice';
 import { useMarkerIcon } from 'helpers/map';
 import { hasDeployedSpotter } from 'helpers/siteUtils';
@@ -17,19 +17,18 @@ import Popup from '../Popup';
 
 // To make sure we can see all the sites all the time, and especially
 // around -180/+180, we create dummy copies of each site.
+// TODO: Add back the functionality
 const LNG_OFFSETS = [-360, 0, 360];
 
 interface SiteMarkerProps {
   site: Site;
-  setCenter: (inputMap: L.Map, latLng: [number, number], zoom: number) => void;
 }
 
 /**
  * All in one site marker with icon, offset duplicates, and popup built in.
  */
-export default function SiteMarker({ site, setCenter }: SiteMarkerProps) {
+export const SiteMarker = React.memo(({ site }: SiteMarkerProps) => {
   const isSelected = useSelector(isSelectedOnMapSelector(site.id));
-  const { map } = useLeaflet();
   const dispatch = useDispatch();
   const { tempWeeklyAlert } = site.collectionData || {};
   const markerIcon = useMarkerIcon(
@@ -43,24 +42,18 @@ export default function SiteMarker({ site, setCenter }: SiteMarkerProps) {
   if (site.polygon.type !== 'Point') return null;
 
   const [lng, lat] = site.polygon.coordinates;
+
   return (
-    <>
-      {LNG_OFFSETS.map((offset) => {
-        return (
-          <Marker
-            onClick={() => {
-              if (map) setCenter(map, [lat, lng], 6);
-              dispatch(setSearchResult());
-              dispatch(setSiteOnMap(site));
-            }}
-            key={`${site.id}-${offset}`}
-            icon={markerIcon}
-            position={[lat, lng + offset]}
-          >
-            {isSelected && <Popup site={site} autoOpen={offset === 0} />}
-          </Marker>
-        );
-      })}
-    </>
+    <Marker
+      onClick={() => {
+        dispatch(setSearchResult());
+        dispatch(setSiteOnMap(site));
+      }}
+      key={`${site.id}`}
+      icon={markerIcon}
+      position={[lat, lng]}
+    >
+      {isSelected && <Popup site={site} />}
+    </Marker>
   );
-}
+});
