3ed20aae17ad5c4f85b9290de75efc462cf6bdbf
Konstantinos Markopoulos <konst.markopoulos@gmail.com>
2024-12-30 10:57:04 +0200

    Fix lint errors

--- packages/website/src/routes/Buoy/index.test.tsx
+++ packages/website/src/routes/Buoy/index.test.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';
 import configureStore from 'redux-mock-store';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { mockUser } from 'mocks/mockUser';
 
 import { renderWithProviders } from 'utils/test-utils';
