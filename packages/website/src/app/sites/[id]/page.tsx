b88b7fb36f82345bab8385acd74b5baf0d2fab55
Konstantinos Markopoulos <konst.markopoulos@gmail.com>
2025-02-10 12:45:25 +0200

    Add pages metadata

--- packages/website/src/app/sites/[id]/page.tsx
+++ packages/website/src/app/sites/[id]/page.tsx
@@ -1,16 +1,41 @@
 import Site from 'routes/SiteRoutes/Site';
 import siteServices from 'services/siteServices';
 
-type PageProps = {
-  params: Promise<{ id: string }>;
+type SiteParams = {
+  id: string;
 };
 
-export const generateStaticParams = async () => {
+const fetchSites = async () => {
   const { data: sites } = await siteServices.getSites();
-  return sites.map(({ id }) => ({ id: `${id}` }));
+  return sites;
 };
 
-export default async function SitePage({ params }: PageProps) {
+export const generateStaticParams = async (): Promise<SiteParams[]> => {
+  return (await fetchSites()).map(({ id }) => ({ id: `${id}` }));
+};
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<SiteParams>;
+}) => {
+  const id = parseInt((await params).id, 10);
+  // We use the same /sites request, instead of site/:id, because the response is cached (axios-cache)
+  const { name } = (await fetchSites()).find((site) => site.id === id) ?? {
+    name: `Site ${id}`,
+  };
+
+  return {
+    title: `Monitoring ${name} | Aqualink dashboard`,
+    description: `View real-time data for ${name}, including reef health, water temperature, wind, and wave conditions. Monitor marine ecosystems with Aqualink.`,
+  };
+};
+
+export default async function SitePage({
+  params,
+}: {
+  params: Promise<SiteParams>;
+}) {
   const { id } = await params;
   return <Site siteId={id} />;
 }
--- packages/website/src/routes/HomeMap/index.tsx
+++ packages/website/src/routes/HomeMap/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useEffect, useState } from 'react';
 import * as React from 'react';
 import { useSearchParams } from 'next/navigation';
