e168784bf43f05ca6c50efcbd7e6e72aff06a38d
ericboucher <eric.p.boucher@gmail.com>
2025-11-29 18:32:56 +0100

    Update p-limit.mock.js

--- packages/api/test/mock/p-limit.mock.js
+++ packages/api/test/mock/p-limit.mock.js
@@ -3,6 +3,7 @@
  * p-limit creates a concurrency limiter function
  * This mock simply executes functions without concurrency limiting for tests
  */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function pLimit(_concurrency) {
   return async (fn) => {
     return fn();
@@ -10,4 +11,3 @@ function pLimit(_concurrency) {
 }
 
 module.exports = pLimit;
-
