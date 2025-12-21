51adee1e51d6b04625237f65fa0e816bade720c6
ericboucher <eric.p.boucher@gmail.com>
2025-12-21 11:22:54 +0000

    Update SeapHOxCard.tsx

--- packages/website/src/common/SiteDetails/SeapHOx/SeapHOxCard.tsx
+++ packages/website/src/common/SiteDetails/SeapHOx/SeapHOxCard.tsx
@@ -68,7 +68,11 @@ function SeapHOxCard({ depth, data, classes }: SeapHOxCardPropsWithStyles) {
         title={
           <Grid container>
             <Grid item>
-              <Typography className={classes.cardTitle} variant="h6" style={{ textTransform: 'uppercase' }}>
+              <Typography
+                className={classes.cardTitle}
+                variant="h6"
+                style={{ textTransform: 'uppercase' }}
+              >
                 SeapHOx at {depth ? `${depth}m` : ''} depth
               </Typography>
             </Grid>
