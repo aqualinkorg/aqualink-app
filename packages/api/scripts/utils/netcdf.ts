b9546abdefff1c2d0d093cd2114522818cdbc275
ericboucher <eric.p.boucher@gmail.com>
2026-04-19 21:57:16 +0200

    Update netcdf.ts

--- packages/api/scripts/utils/netcdf.ts
+++ packages/api/scripts/utils/netcdf.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
 /* eslint-disable global-require */
 import { times } from 'lodash';
 import { DateTime } from 'luxon';
