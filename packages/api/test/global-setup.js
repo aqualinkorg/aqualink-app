46919b81ed9176a3df8b5d8122d6327a546fb344
Tristan Tang <tristan@jovohintl.com>
2026-05-31 04:51:50 +0800

    Make API tests resilient without external credentials

--- packages/api/test/global-setup.js
+++ packages/api/test/global-setup.js
@@ -1,3 +1,7 @@
 module.exports = async () => {
   process.env.TZ = 'UTC';
+  process.env.BACKEND_BASE_URL =
+    process.env.BACKEND_BASE_URL || 'http://localhost:8080';
+  process.env.STORAGE_MAX_FILE_SIZE_MB =
+    process.env.STORAGE_MAX_FILE_SIZE_MB || '25';
 };
