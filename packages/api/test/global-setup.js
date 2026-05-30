914e259f6ed38c5d07110f96f4bc1a4d573c5d02
Tristan Tang <tristan@jovohintl.com>
2026-05-31 05:04:29 +0800

    Set test storage bucket default

--- packages/api/test/global-setup.js
+++ packages/api/test/global-setup.js
@@ -4,4 +4,5 @@ module.exports = async () => {
     process.env.BACKEND_BASE_URL || 'http://localhost:8080';
   process.env.STORAGE_MAX_FILE_SIZE_MB =
     process.env.STORAGE_MAX_FILE_SIZE_MB || '25';
+  process.env.GCS_BUCKET = process.env.GCS_BUCKET || 'storage';
 };
