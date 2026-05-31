41d36bcd2150b58fcfd400eb25797e4d8d509f96
dung11122d <116857270+dung11122d@users.noreply.github.com>
2026-05-31 17:58:49 +0700

    Make API tests resilient in fork CI

--- packages/api/test/global-setup.js
+++ packages/api/test/global-setup.js
@@ -1,3 +1,8 @@
 module.exports = async () => {
   process.env.TZ = 'UTC';
+  process.env.BACKEND_BASE_URL =
+    process.env.BACKEND_BASE_URL || 'http://localhost:8080';
+  process.env.STORAGE_MAX_FILE_SIZE_MB =
+    process.env.STORAGE_MAX_FILE_SIZE_MB || '25';
+  process.env.GCS_BUCKET = process.env.GCS_BUCKET || 'storage';
 };
