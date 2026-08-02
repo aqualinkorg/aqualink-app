edf950952e70f0d0ab7ab721262fa86d758bf4d9
Franco Di Gregorio <127508658+FrancoLDG@users.noreply.github.com>
2026-08-02 04:29:48 -0300

    feat: view site data for a historical date

--- packages/api/src/utils/collections.utils.ts
+++ packages/api/src/utils/collections.utils.ts
@@ -5,6 +5,7 @@ import { CollectionDataDto } from '../collections/dto/collection-data.dto';
 import { Site } from '../sites/sites.entity';
 import { SourceType } from '../sites/schemas/source-type.enum';
 import { LatestData } from '../time-series/latest-data.entity';
+import { TimeSeries } from '../time-series/time-series.entity';
 
 export const getCollectionData = async (
   sites: Site[],
@@ -36,6 +37,52 @@ export const getCollectionData = async (
     .mapValues<CollectionDataDto>((data) =>
       data.reduce<CollectionDataDto>(
         (acc, siteData): CollectionDataDto => ({
+          ...acc,
+          [camelCase(siteData.metric)]: siteData.value,
+        }),
+        {},
+      ),
+    )
+    .toJSON();
+};
+
+export const getHistoricalCollectionData = async (
+  sites: Site[],
+  timeSeriesRepository: Repository<TimeSeries>,
+  date: Date,
+): Promise<Record<number, CollectionDataDto>> => {
+  const siteIds = sites.map((site) => site.id);
+
+  if (!siteIds.length) {
+    return {};
+  }
+
+  const historicalData = await timeSeriesRepository
+    .createQueryBuilder('time_series')
+    .select(
+      'DISTINCT ON (time_series.metric, source.type, source.site_id, source.survey_point_id) time_series.id',
+    )
+    .addSelect('time_series.timestamp', 'timestamp')
+    .addSelect('time_series.value', 'value')
+    .addSelect('source.site_id', 'siteId')
+    .addSelect('source.survey_point_id', 'surveyPointId')
+    .addSelect('time_series.metric', 'metric')
+    .innerJoin('time_series.source', 'source')
+    .where('source.site_id IN (:...siteIds)', { siteIds })
+    .andWhere('source.type != :hoboSource', { hoboSource: SourceType.HOBO })
+    .andWhere('time_series.timestamp <= :date', { date })
+    .orderBy('time_series.metric', 'ASC')
+    .addOrderBy('source.type', 'ASC')
+    .addOrderBy('source.site_id', 'ASC')
+    .addOrderBy('source.survey_point_id', 'ASC')
+    .addOrderBy('time_series.timestamp', 'DESC')
+    .getRawMany();
+
+  return _(historicalData)
+    .groupBy((data) => data.siteId)
+    .mapValues<CollectionDataDto>((data) =>
+      data.reduce<CollectionDataDto>(
+        (acc, siteData) => ({
           ...acc,
           [camelCase(siteData.metric)]: siteData.value,
         }),
